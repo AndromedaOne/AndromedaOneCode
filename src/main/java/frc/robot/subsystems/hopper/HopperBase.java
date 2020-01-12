815213cf2ecb41ed67636ecb9bfbad48ac4bf9c2
EthanM827 <ethanmcfetridge@gmail.com>
2020-01-12 13:38:30 -0500

    Added mock subsystems for hopper, intake, and shooter

--- src/main/java/frc/robot/subsystems/hopper/HopperBase.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperBase.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.hopper;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public abstract class HopperBase extends SubsystemBase {
-    public abstract void ballInHopperPos(int position);
+    public abstract boolean ballInHopperPos(int position);
 
     public abstract void turnHopperNumPositions(double positions);
 
