7f7d97ea908eb3e2daff9bf6a92d4f8e44e12b68
SPD3 <sean@ayerdoyles.org>
2020-01-16 16:25:10 -0500

    Added new packages and made robot a singleton
    Also adde the telemetries code from last year. I put dummy files into each new package, but we can get rid of those later.

---
+++ src/main/java/frc/robot/sensors/ExampleSensor.java
@@ -0,0 +1,9 @@
+package frc.robot.sensors;
+
+public class ExampleSensor {
+
+    public double getSensorValue() {
+        System.out.println("Getting example sensor's measurement");
+        return 0;
+    }
+}
