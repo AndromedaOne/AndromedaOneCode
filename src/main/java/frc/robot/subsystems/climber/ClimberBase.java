52d6c84afd31dd5886c20b5c5282725a2cc589f1
Steeman <kjell.steeman@gmail.com>
2020-01-23 20:00:39 -0500

    I was adding packages for the architecture.

--- src/main/java/frc/robot/subsystems/climber/ClimberBase.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberBase.java
@@ -1,19 +1,19 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.climber;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
+public class ClimberBase extends SubsystemBase {
   /**
-   * Creates a new ExampleSubsystem.
+   * Creates a new ClimberBase.
    */
-  public ExampleSubsystem() {
+  public ClimberBase() {
 
   }
 
