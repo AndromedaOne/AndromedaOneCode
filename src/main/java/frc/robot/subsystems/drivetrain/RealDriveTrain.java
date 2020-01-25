a9850cae63e883062b04e2e52cc6143ce26315a4
Steeman <kjell.steeman@gmail.com>
2020-01-25 14:39:35 -0500

    I added a 2 speed controller using left and right.

--- src/main/java/frc/robot/subsystems/drivetrain/RealDriveTrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/RealDriveTrain.java
@@ -7,20 +7,30 @@
 
 package frc.robot.subsystems.drivetrain;
 
-public class RealDriveTrain extends DriveTrain {
-  /**
-   * Creates a new VBusControl.
-   */
-  public RealDriveTrain() {
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.actuators.SparkMaxController;
 
-  }
+public class RealDriveTrain extends DriveTrain {
+  // public static SparkMaxController
+
+  // motors on the Left side of the drive
+  private final SpeedControllerGroup m_leftmotors = new SpeedControllerGroup(new SparkMaxController("frontleft"),
+      new SparkMaxController("backleft"));
+
+  // motors on the right side of the drive
+  private final SpeedControllerGroup m_rightmotors = new SpeedControllerGroup(new SparkMaxController("frontright"),
+      new SparkMaxController("backright"));
+
+  // the robot's main drive
+  private final DifferentialDrive m_drive = new DifferentialDrive(m_leftmotors, m_rightmotors);
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
 
-  public void move(double forwardBackSpeed, double rotateAmount, boolean squaredInput) {
-
+  public void move(final double forwardBackSpeed, final double rotateAmount, final boolean squaredInput) {
+    m_drive.arcadeDrive(forwardBackSpeed, rotateAmount);
   }
 }
