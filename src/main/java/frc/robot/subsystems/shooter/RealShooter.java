99daa8cac9545e640e64f99e7119aa9de7344286
Andromeda One LAW-1 Laptop <andromedaonesoftwareteam@googlegroups.com>
2020-01-30 20:43:15 -0500

    added closing and opening shooter hood methods

--- src/main/java/frc/robot/subsystems/shooter/RealShooter.java
+++ src/main/java/frc/robot/subsystems/shooter/RealShooter.java
@@ -3,18 +3,19 @@ package frc.robot.subsystems.shooter;
 import com.typesafe.config.Config;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import frc.robot.actuators.*;
-
 import frc.robot.Config4905;
+import frc.robot.actuators.*;
 
 public class RealShooter extends ShooterBase {
 
   private SparkMaxController m_shooterOne;
   private SparkMaxController m_shooterTwo;
   private SpeedControllerGroup m_shooterGroup;
-   // TODO Add Talon And pneumatic Controllers
+  private DoubleSolenoid4905 m_shooterHoodOne;
+  private DoubleSolenoid4905 m_shooterHoodTwo;
+  // TODO Add Talon And pneumatic Controllers
 
-  public RealShooter(){
+  public RealShooter() {
     Config shooterConfig = Config4905.getConfig4905().getShooterConfig();
 
     m_shooterOne = new SparkMaxController(shooterConfig, "shooterone");
@@ -22,6 +23,10 @@ public class RealShooter extends ShooterBase {
     m_shooterTwo = new SparkMaxController(shooterConfig, "shootertwo");
 
     m_shooterGroup = new SpeedControllerGroup(m_shooterOne, m_shooterTwo);
+
+    m_shooterHoodOne = new DoubleSolenoid4905(shooterConfig, "hoodone");
+
+    m_shooterHoodTwo = new DoubleSolenoid4905(shooterConfig, "hoodtwo");
 
   }
 
@@ -44,14 +49,14 @@ public class RealShooter extends ShooterBase {
 
   @Override
   public void openShooterHood() {
-    // TODO Auto-generated method stub
-
+    m_shooterHoodOne.extendPiston();
+    m_shooterHoodTwo.extendPiston();
   }
 
   @Override
   public void closeShooterHood() {
-    // TODO Auto-generated method stub
-
+    m_shooterHoodOne.retractPiston();
+    m_shooterHoodTwo.retractPiston();
   }
 
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterBase.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterBase.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public abstract class ShooterBase extends SubsystemBase {
 
   public abstract double getShooterVelocity();
-  
+
   public abstract void setShooterPower(double power);
 
   public abstract double getShooterPower();
