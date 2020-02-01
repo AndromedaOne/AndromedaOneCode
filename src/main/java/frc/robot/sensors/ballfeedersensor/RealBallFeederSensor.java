12964f30c311a3c474274de768c0e9701193a432
Owen Salter <owen@devosmium.xyz>
2020-02-01 15:00:02 -0500

    feat(feeder): Make member variables have "m_"

--- src/main/java/frc/robot/sensors/ballfeedersensor/RealBallFeederSensor.java
+++ src/main/java/frc/robot/sensors/ballfeedersensor/RealBallFeederSensor.java
@@ -8,44 +8,44 @@ import frc.robot.Config4905;
 
 public class RealBallFeederSensor extends BallFeederSensorBase {
 
-  boolean isOnboard;
-  private static I2C i2c;
-  private byte[] dataBuffer;
-  private int numSensors;
-  private int detectionThreshold;
+  boolean m_isOnboard;
+  private static I2C m_i2c;
+  private byte[] m_dataBuffer;
+  private int m_numSensors;
+  private int m_detectionThreshold;
 
   public RealBallFeederSensor(String confString) {
     Config conf = Config4905.getConfig4905().getSensorConfig();
     Config lineConf = conf.getConfig("sensors." + confString);
-    isOnboard = lineConf.getBoolean("isOnboard");
-    if (isOnboard) {
-      i2c = new I2C(I2C.Port.kOnboard, 2);
+    m_isOnboard = lineConf.getBoolean("isOnboard");
+    if (m_isOnboard) {
+      m_i2c = new I2C(I2C.Port.kOnboard, 2);
     } else {
-      i2c = new I2C(I2C.Port.kMXP, 2);
+      m_i2c = new I2C(I2C.Port.kMXP, 2);
     }
-    numSensors = lineConf.getInt("numSensors");
-    dataBuffer = new byte[2 * numSensors];
-    detectionThreshold = lineConf.getInt("detectionThreshold");
-    SmartDashboard.putRaw("ballFeederSensor", dataBuffer);
+    m_numSensors = lineConf.getInt("numSensors");
+    m_dataBuffer = new byte[2 * m_numSensors];
+    m_detectionThreshold = lineConf.getInt("detectionThreshold");
+    SmartDashboard.putRaw("ballFeederSensor", m_dataBuffer);
   }
 
   @Override
   void getSensorReading() {
     // TODO Auto-generated method stub
-    i2c.readOnly(dataBuffer, 2 * numSensors);
+    m_i2c.readOnly(m_dataBuffer, 2 * m_numSensors);
   }
 
   public boolean[] isThereBall() {
-    boolean[] boolBuf = new boolean[dataBuffer.length / 2];
-    double[] dValues = new double[dataBuffer.length / 2];
+    boolean[] boolBuf = new boolean[m_dataBuffer.length / 2];
+    double[] dValues = new double[m_dataBuffer.length / 2];
 
-    i2c.readOnly(dataBuffer, (numSensors * 2) - 1);
+    m_i2c.readOnly(m_dataBuffer, (m_numSensors * 2) - 1);
     // Step through each even-numbered element in the array
-    for (int i = 0; i < dataBuffer.length / 2; i++) {
-      if (dataBuffer[i * 2] >= 0) {
-        dValues[i] = dataBuffer[i * 2];
+    for (int i = 0; i < m_dataBuffer.length / 2; i++) {
+      if (m_dataBuffer[i * 2] >= 0) {
+        dValues[i] = m_dataBuffer[i * 2];
       } else {
-        dValues[i] = dataBuffer[i * 2] + 256;
+        dValues[i] = m_dataBuffer[i * 2] + 256;
       }
 
     }
@@ -58,7 +58,7 @@ public class RealBallFeederSensor extends BallFeederSensorBase {
     System.out.println(values);
     // Check for whether the line is found
     for (int i = 0; i < dValues.length; i++) {
-      if (dValues[i] >= detectionThreshold) {
+      if (dValues[i] >= m_detectionThreshold) {
         boolBuf[i] = true;
       } else {
         boolBuf[i] = false;
