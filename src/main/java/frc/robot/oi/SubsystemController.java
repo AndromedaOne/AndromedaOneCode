6c4dd268759adedb06b3820d1c036ae6c0a4b3f0
Steeman <kjell.steeman@gmail.com>
2020-02-01 16:53:45 -0500

    We started adding for controlling intake JoyStick

--- src/main/java/frc/robot/oi/SubsystemController.java
+++ src/main/java/frc/robot/oi/SubsystemController.java
@@ -7,11 +7,15 @@
 
 package frc.robot.oi;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * Add your docs here.
  */
 public class SubsystemController {
   private XboxController m_subsystemController = new XboxController(1);
+  // public JoystickButton getDeployRunIntakeButton() {
+ // }
 }
