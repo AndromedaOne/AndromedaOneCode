67b915ea3d5f3a02b1725200f9e02673e0165b8f
Owen Salter <owen@devosmium.xyz>
2020-02-01 16:39:02 -0500

    feat(controlpanel): Add control panel classes and stub methods
                        RealControlPanel still has things to be fleshed out

---
+++ src/main/java/frc/robot/subsystems/controlpanel/ControlPanelBase.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems.controlpanel;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public abstract class ControlPanelBase extends SubsystemBase {
+  /**
+   * Creates a new ControlPanelBase.
+   */
+  public ControlPanelBase() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public abstract void extendSystem();
+
+  public abstract void retractSystem();
+
+  public abstract void rotateOneTime();
+
+  public abstract void rotateToColor();
+}
---
+++ src/main/java/frc/robot/subsystems/controlpanel/MockControlPanel.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.controlpanel;
+
+public class MockControlPanel extends ControlPanelBase {
+
+    @Override
+    public void extendSystem() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void retractSystem() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void rotateOneTime() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void rotateToColor() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
