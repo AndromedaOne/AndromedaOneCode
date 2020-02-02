681c27e55c2fe81fe703eb0b84111c8d6a547939
FRC4905 <frc4905software@gmail.com>
2020-02-02 16:34:36 -0500

    Finished shooter group command.

--- src/main/java/frc/robot/groupcommands/parallelgroup/DefaultShooterParallelCommandGroup.java
+++ src/main/java/frc/robot/groupcommands/parallelgroup/DefaultShooterParallelCommandGroup.java
@@ -1,22 +1,20 @@
-package frc.robot.groupcommands;
+package frc.robot.groupcommands.parallelgroup;
 
 import com.typesafe.config.Config;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Config4905;
 import frc.robot.commands.pidcommands.RunShooterSeriesVelocity;
 import frc.robot.commands.pidcommands.RunShooterWheelVelocity;
 import frc.robot.subsystems.shooter.ShooterBase;
 
-public class ShooterParallelCommandGroup extends ParallelCommandGroup {
+public class DefaultShooterParallelCommandGroup extends ParallelCommandGroup {
     private Config m_shooterConfig = Config4905.getConfig4905().getShooterConfig();
     private final double m_seriesIdleSpeed = m_shooterConfig.getDouble("seriesidlespeed");
     private final double m_shooterIdleSpeed = m_shooterConfig.getDouble("shooteridlespeed");
     private ShooterBase m_shooter;
 
-    public ShooterParallelCommandGroup(ShooterBase shooter) {
+    public DefaultShooterParallelCommandGroup(ShooterBase shooter) {
         m_shooter = shooter;
          }
 
