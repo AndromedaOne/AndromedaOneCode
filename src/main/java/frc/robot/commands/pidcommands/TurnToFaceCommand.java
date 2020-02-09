c1e96739a2056b6397a4d11c46e705c788f3f859
Zach Malstrom <zacharymalstrom@gmail.com>
2020-02-08 20:46:31 -0500

    Finished adding keybindings to the ttf command

--- src/main/java/frc/robot/commands/pidcommands/TurnToFaceCommand.java
+++ src/main/java/frc/robot/commands/pidcommands/TurnToFaceCommand.java
@@ -2,30 +2,29 @@ package frc.robot.commands.pidcommands;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.Robot;
+import frc.robot.pidcontroller.PIDCommand4905;
+import frc.robot.pidcontroller.PIDController4905;
 
 /**
  * Limelight Turn To Face Command.
  */
-public class TurnToFaceCommand extends PIDCommand {
+public class TurnToFaceCommand extends PIDCommand4905 {
   DoubleSupplier sensor;
   boolean lastSetpoint = false;
 
   public TurnToFaceCommand(DoubleSupplier sensor) {
-    super(new PIDController(3e-2, 8e-3, 5e-4), sensor, 0.0, TurnToFaceCommand::stub);
-    this.getController().setTolerance(3.0);
+    super(new PIDController4905("Turn To Face PID", 3e-2, 8e-3, 5e-4, 0.2), sensor, 0.0, 
+      (lambda) -> Robot.getInstance().getSubsystemsContainer().getDrivetrain().moveUsingGyro(0.0, -lambda, true, true));
+    this.getController().setTolerance(0.1);
     this.sensor = sensor;
   }
 
-  public static void stub(double d) {
-    System.out.println(d);
-  }
 
   @Override
   public boolean isFinished() {
-    double angle = sensor.getAsDouble();// NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0.0);
-    System.out.println(angle);
+    double angle = sensor.getAsDouble();
+    System.out.println(angle +" "+this.getController().atSetpoint());
     boolean returnValue = this.getController().atSetpoint() && lastSetpoint;
     lastSetpoint = this.getController().atSetpoint();
     return returnValue;
