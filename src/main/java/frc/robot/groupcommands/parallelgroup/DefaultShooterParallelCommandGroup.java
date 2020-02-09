08a62715f0dbabd938e08a6e9373cb4702a1733a
Devin <dcannava02@gmail.com>
2020-02-08 20:06:22 -0500

    Errors wooooo

--- src/main/java/frc/robot/groupcommands/parallelgroup/DefaultShooterParallelCommandGroup.java
+++ src/main/java/frc/robot/groupcommands/parallelgroup/DefaultShooterParallelCommandGroup.java
@@ -8,21 +8,28 @@ import frc.robot.commands.pidcommands.RunShooterWheelVelocity;
 import frc.robot.subsystems.shooter.ShooterBase;
 
 public class DefaultShooterParallelCommandGroup extends ParallelCommandGroup {
-    private Config m_shooterConfig = Config4905.getConfig4905().getShooterConfig();
+    private Config m_shooterConfig;
     private double m_shooterIdleSpeed;
     private double m_seriesIdleSpeed;
     private ShooterBase m_shooter;
 
     public DefaultShooterParallelCommandGroup(ShooterBase shooter) {
+        m_shooterConfig = Config4905.getConfig4905().getShooterConfig();
         m_shooterIdleSpeed = m_shooterConfig.getDouble("shooteridlespeed");
         m_seriesIdleSpeed = m_shooterConfig.getDouble("seriesidlespeed");
         m_shooter = shooter;
+        addRequirements(shooter);
     }
 
     @Override
     public void initialize() {
         addCommands(new RunShooterSeriesVelocity(m_shooter, m_seriesIdleSpeed),
          new RunShooterWheelVelocity(m_shooter, m_shooterIdleSpeed));
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
     }
 
 }
