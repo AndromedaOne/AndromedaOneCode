83aa3dd5e18b6abe1ef863f716f78a524695f86c
EthanM827 <ethanmcfetridge@gmail.com>
2020-02-09 13:59:52 -0500

    Working DelayedSequentialCommandGroup

--- src/main/java/frc/robot/oi/SmartDashboard4905.java
+++ src/main/java/frc/robot/oi/SmartDashboard4905.java
@@ -10,14 +10,13 @@ package frc.robot.oi;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.DoNothingAuto;
 import frc.robot.commands.DriveBackwardTimed;
 import frc.robot.commands.ShooterCommand;
 import frc.robot.commands.pidcommands.MoveUsingEncoder;
 import frc.robot.commands.pidcommands.TurnDeltaAngle;
 import frc.robot.commands.pidcommands.TurnToCompassHeading;
+import frc.robot.groupcommands.sequentialgroup.DelayedSequentialCommandGroup;
 import frc.robot.subsystems.SubsystemsContainer;
 import frc.robot.subsystems.drivetrain.DriveTrain;
 import frc.robot.subsystems.intake.IntakeBase;
@@ -58,23 +57,16 @@ public class SmartDashboard4905 {
     // @formatter:off
     m_autoChooser.setDefaultOption("DoNothing", 
                                    new DoNothingAuto());
-    m_autoChooser.addOption("Strategy1", 
-                            new MoveUsingEncoder(driveTrain, -12));
-    m_autoChooser.addOption("Wait, Strategy1",
-                            new SequentialCommandGroup(new WaitCommand(3),
-                                                       new MoveUsingEncoder(driveTrain, -12)));
+    m_autoChooser.addOption("Strategy1",
+                            new DelayedSequentialCommandGroup(new MoveUsingEncoder(driveTrain, -12)));
     m_autoChooser.addOption("Strategy2",
-                            new SequentialCommandGroup(new MoveUsingEncoder(driveTrain, -12), 
-                                                       new ShooterCommand(shooter, 3)));
-    m_autoChooser.addOption("Wait, Strategy2", 
-                            new SequentialCommandGroup(new WaitCommand(3),
-                                                       new MoveUsingEncoder(driveTrain, -12), 
-                                                       new ShooterCommand(shooter, 3)));
+                            new DelayedSequentialCommandGroup(new ShooterCommand(shooter, 3),
+                                                              new MoveUsingEncoder(driveTrain, -12)));
     m_autoChooser.addOption("Strategy3", 
-                            new SequentialCommandGroup(new ShooterCommand(shooter, 3)));
+                            new DelayedSequentialCommandGroup(new ShooterCommand(shooter, 3)));
     m_autoChooser.addOption("Strategy4", 
-                            new SequentialCommandGroup(new ShooterCommand(shooter, 3), 
-                                                       new MoveUsingEncoder(driveTrain, 60)));
+                            new DelayedSequentialCommandGroup(new ShooterCommand(shooter, 3), 
+                                                              new MoveUsingEncoder(driveTrain, 60)));
     SmartDashboard.putData("autoModes", m_autoChooser);
     // @formatter:on
   }
