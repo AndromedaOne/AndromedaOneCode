a1340e039900f8ff08448070ef8f9d730e62611d
SPD3 <sean@ayerdoyles.org>
2020-02-09 16:12:36 -0500

    Added methods into feedBothstagesintoshooter command to run feeder

--- src/main/java/frc/robot/commands/FeedBothStagesIntoShooter.java
+++ src/main/java/frc/robot/commands/FeedBothStagesIntoShooter.java
@@ -29,11 +29,13 @@ public class FeedBothStagesIntoShooter extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    m_feederBase.driveBothStages();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_feederBase.stopBothStages();
   }
 
   // Returns true when the command should end.
