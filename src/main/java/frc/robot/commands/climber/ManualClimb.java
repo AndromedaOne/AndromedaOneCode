2a1ad2d22905b7687272934aab9595db264a1252
Owen Salter <owen@devosmium.xyz>
2020-02-09 14:58:09 -0500

    feat(climber): Fill out end method

--- src/main/java/frc/robot/commands/climber/ManualClimb.java
+++ src/main/java/frc/robot/commands/climber/ManualClimb.java
@@ -49,6 +49,7 @@ public class ManualClimb extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    climber.stopWinch();
   }
 
   // Returns true when the command should end.
