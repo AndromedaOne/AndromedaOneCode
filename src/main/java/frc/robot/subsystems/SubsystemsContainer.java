fc2c064aaa785459f949da036c2d3f7189368fd1
SPD3 <sean@ayerdoyles.org>
2020-02-11 18:51:30 -0500

    Changed feeder default command to do nothing because we won't have the right sensors for week 0. Told the shooter that it's kicker wheel does not have an encoder. Added configuration  code for the intake.

--- src/main/java/frc/robot/subsystems/SubsystemsContainer.java
+++ src/main/java/frc/robot/subsystems/SubsystemsContainer.java
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import frc.robot.Config4905;
 import frc.robot.commands.DefaultFeederCommand;
+import frc.robot.commands.DefaultFeederCommandWeek0;
 import frc.robot.commands.RetractIntake;
 import frc.robot.commands.TeleOpCommand;
 import frc.robot.commands.TeleopClimber;
@@ -138,7 +139,7 @@ public class SubsystemsContainer {
     m_driveTrain.setDefaultCommand(new TeleOpCommand());
     m_shooter.setDefaultCommand(new DefaultShooterParallelCommandGroup(m_shooter));
     m_intake.setDefaultCommand(new RetractIntake(m_intake));
-    m_feeder.setDefaultCommand(new DefaultFeederCommand());
+    m_feeder.setDefaultCommand(new DefaultFeederCommandWeek0());
     m_climber.setDefaultCommand(new TeleopClimber(m_climber));
   }
 }
