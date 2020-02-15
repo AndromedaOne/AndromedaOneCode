b26a85dc6390a8f83cd21d3197c3f7f9c8c2852f
Devin <dcannava02@gmail.com>
2020-02-15 14:47:40 -0500

    stuff that we did at week zero

--- src/main/java/frc/robot/commands/DefaultFeederCommand.java
+++ src/main/java/frc/robot/commands/DefaultFeederCommand.java
@@ -32,6 +32,7 @@ public class DefaultFeederCommand extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    Robot.getInstance().getSubsystemsContainer().getShooter().closeShooterHood();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
