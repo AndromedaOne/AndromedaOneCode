1a26c111b46a134ef254e0bcd3886398c72c4868
themccubbins <themccubbins@gmail.com>
2020-02-16 15:39:51 -0500

    Added prettyprint method for ball feeder

--- src/main/java/frc/robot/sensors/ballfeedersensor/EnumBallLocation.java
+++ src/main/java/frc/robot/sensors/ballfeedersensor/EnumBallLocation.java
@@ -16,4 +16,25 @@ public enum EnumBallLocation {
   public int getIndex() {
     return index;
   }
+  
+  public static String prettyPrint(boolean[] sensorOutput){
+    StringBuffer buf = new StringBuffer();
+    buf.append(sensorOutput[STAGE_1_LEFT.getIndex()] ? "1-L" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_1_RIGHT.getIndex()] ? "1-R" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_1_END.getIndex()] ? "1-E" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_2_BEGINNING.getIndex()] ? "2-B" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_2_BEGINNING_MiDDLE.getIndex()] ? "2MB" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_2_MIDDLE.getIndex()] ? "2-M" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_2_END_MIDDLE.getIndex()] ? "2ME" : "---");
+    buf.append(" ");
+    buf.append(sensorOutput[STAGE_1_END.getIndex()] ? "2-E" : "---");
+    buf.append(" ");
+    return buf.toString()
+  }
 }
