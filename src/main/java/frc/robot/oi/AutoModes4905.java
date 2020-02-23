7dbce40fdd662fd304a61d1d6bcd35dabaf9406d
EthanM827 <ethanmcfetridge@gmail.com>
2020-02-23 14:48:56 -0500

    Removed TurnAndShoot, because according to R&S we should always be in a known position to shoot in auto

--- src/main/java/frc/robot/oi/AutoModes4905.java
+++ src/main/java/frc/robot/oi/AutoModes4905.java
@@ -7,8 +7,8 @@ import frc.robot.commands.DoNothingAuto;
 import frc.robot.commands.pidcommands.MoveUsingEncoder;
 import frc.robot.commands.pidcommands.TurnToCompassHeading;
 import frc.robot.groupcommands.DeployAndRunIntake;
+import frc.robot.groupcommands.parallelgroup.ShootWithDistance;
 import frc.robot.groupcommands.sequentialgroup.DelayedSequentialCommandGroup;
-import frc.robot.groupcommands.sequentialgroup.TurnAndShoot;
 import frc.robot.sensors.SensorsContainer;
 import frc.robot.subsystems.SubsystemsContainer;
 import frc.robot.subsystems.drivetrain.DriveTrain;
@@ -33,10 +33,10 @@ public class AutoModes4905 {
         m_autoChooser.addOption("1: Move Back",
                                 new DelayedSequentialCommandGroup(new MoveUsingEncoder(driveTrain, -12)));
         m_autoChooser.addOption("2: Fire and Move Back",
-                                new DelayedSequentialCommandGroup(new TurnAndShoot(shooter, feeder, sensorsContainer, 3, (10*12)),
+                                new DelayedSequentialCommandGroup(new ShootWithDistance(shooter, feeder, (10*12)),
                                                                   new MoveUsingEncoder(driveTrain, -12)));
         m_autoChooser.addOption("3: Back Bumper U-Turn", 
-                                new DelayedSequentialCommandGroup(new TurnAndShoot(shooter, feeder, sensorsContainer, 3, (10*12)),
+                                new DelayedSequentialCommandGroup(new ShootWithDistance(shooter, feeder, (10*12)),
                                                                   new MoveUsingEncoder(driveTrain, 30),
                                                                   new TurnToCompassHeading(270),
                                                                   new MoveUsingEncoder(driveTrain, 60),
@@ -44,34 +44,37 @@ public class AutoModes4905 {
                                                                   new MoveUsingEncoder(driveTrain, 126)));
         m_autoChooser.addOption("4: Shoot and Trench Run", 
                                 new DelayedSequentialCommandGroup(new TurnToCompassHeading(334.5),
-                                                                  new TurnAndShoot(shooter, feeder, sensorsContainer, 3, 0), // do math to figure out distance here
+                                                                  new ShootWithDistance(shooter, feeder, 0), // do math to figure out distance here
                                                                   new TurnToCompassHeading(180),
                                                                   new DeployAndRunIntake(intake, () -> true),
                                                                   new MoveUsingEncoder(driveTrain, 249)));
         m_autoChooser.addOption("5: Right Side Shield",
-                                new DelayedSequentialCommandGroup(new TurnAndShoot(shooter, feeder, sensorsContainer, 3, (10*12)),
+                                new DelayedSequentialCommandGroup(new ShootWithDistance(shooter, feeder, (10*12)),
                                                                   new MoveUsingEncoder(driveTrain, -69),
                                                                   new TurnToCompassHeading(270),
                                                                   new DeployAndRunIntake(intake, () -> true),
                                                                   new MoveUsingEncoder(driveTrain, 12))); // Waiting on official distance to move here from R&S
         m_autoChooser.addOption("6: Left Side Shield", 
-                                new DelayedSequentialCommandGroup(new TurnAndShoot(shooter, feeder, sensorsContainer, 3, (10*12)),
+                                new DelayedSequentialCommandGroup(new ShootWithDistance(shooter, feeder, (10*12)),
                                                                   new MoveUsingEncoder(driveTrain, (2*12)+6),
                                                                   new TurnToCompassHeading(270),
                                                                   new MoveUsingEncoder(driveTrain, (5*12)),
                                                                   new TurnToCompassHeading(180),
                                                                   new DeployAndRunIntake(intake, () -> true),
                                                                   new MoveUsingEncoder(driveTrain, (10*12) + 6)));
-        m_autoChooser.addOption("7: Enemy Trench Run", 
+        // Supposedly never going to be used, according to R&S, but kept to keep numbering system intact
+        if (false) {
+          m_autoChooser.addOption("7: Enemy Trench Run", 
                                 new DelayedSequentialCommandGroup(new DeployAndRunIntake(intake, () -> true),
                                                                   new MoveUsingEncoder(driveTrain, (23*12)+9)));
+        }
         m_autoChooser.addOption("8: Right Fire Move Back",
                                 new DelayedSequentialCommandGroup(new TurnToCompassHeading(350.5),
-                                                                  new TurnAndShoot(shooter, feeder, sensorsContainer, 3, (10*12)),
+                                                                  new ShootWithDistance(shooter, feeder, (10*12)),
                                                                   new MoveUsingEncoder(driveTrain, -12)));
         m_autoChooser.addOption("9: Left Fire Move Back",
                                 new DelayedSequentialCommandGroup(new TurnToCompassHeading(9.5),
-                                                                  new TurnAndShoot(shooter, feeder, sensorsContainer, 3, (10*12)),
+                                                                  new ShootWithDistance(shooter, feeder, (10*12)),
                                                                   new MoveUsingEncoder(driveTrain, -12)));
         SmartDashboard.putData("autoModes", m_autoChooser);
         // @formatter:on
