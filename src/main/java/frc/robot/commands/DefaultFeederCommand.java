038be81a7fb8ca5f7f22e97e3e0437a702106219
SPD3 <sean@ayerdoyles.org>
2020-02-25 23:28:03 -0500

    Change the feeder state to third loaded when running feeder in reverse because that is the safest state to be in.

--- src/main/java/frc/robot/commands/DefaultFeederCommand.java
+++ src/main/java/frc/robot/commands/DefaultFeederCommand.java
@@ -20,7 +20,7 @@ public class DefaultFeederCommand extends CommandBase {
 
   BallFeederSensorBase m_feederSensor;
   private FeederBase m_feeder;
-  private FeederStates m_feederState;
+  private static FeederStates feederState;
   private static final double DEFAULT_STAGES_ONE_AND_TWO_SPEED = 0.3;
   private static final double DEFAULT_DIFFERENCE_STAGE_TWO_AND_THREE_SPEED = 0.0;
   private static final double DEFAULT_STAGE_THREE_SPEED = DEFAULT_STAGES_ONE_AND_TWO_SPEED
@@ -28,7 +28,6 @@ public class DefaultFeederCommand extends CommandBase {
   private static final double STAGE_TWO_SLOW_SPEED = 0.15;
   private static final double STAGE_THREE_SLOW_SPEED = STAGE_TWO_SLOW_SPEED
       + DEFAULT_DIFFERENCE_STAGE_TWO_AND_THREE_SPEED;
-  private FeederStates m_previousState;
   private int emptyCounter = 0;
   private static int numberOfPowerCellsInFeeder = 0;
   private int m_stageOneEndSensorTriggeredCounter = 0;
@@ -42,14 +41,14 @@ public class DefaultFeederCommand extends CommandBase {
     addRequirements(Robot.getInstance().getSubsystemsContainer().getFeeder());
     this.m_feeder = Robot.getInstance().getSubsystemsContainer().getFeeder();
     m_feederSensor = Robot.getInstance().getSensorsContainer().getBallFeederSensor();
-    m_feederState = FeederStates.EMPTY;
+    feederState = FeederStates.EMPTY;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     Robot.getInstance().getSubsystemsContainer().getShooter().closeShooterHood();
-    m_feederState = FeederStates.THIRD_LOADED;
+    feederState = FeederStates.THIRD_LOADED;
     Trace.getInstance().logCommandStart("DefaultFeederCommand");
     m_previousState = null;
     emptyCounter = 0;
@@ -63,7 +62,7 @@ public class DefaultFeederCommand extends CommandBase {
   public void execute() {
     numberOfPowerCellsInFeeder = 0;
     if (Robot.getInstance().isDisabled()) {
-      m_feederState = FeederStates.EMPTY;
+      feederState = FeederStates.EMPTY;
       return;
     }
 
@@ -81,13 +80,13 @@ public class DefaultFeederCommand extends CommandBase {
       emptyCounter = 0;
     }
     if (emptyCounter >= 5) {
-      m_feederState = FeederStates.EMPTY;
+      feederState = FeederStates.EMPTY;
     }
-    switch (m_feederState) {
+    switch (feederState) {
     case EMPTY:
       if (ballSensorValues[STAGE_1_LEFT.getIndex()] || ballSensorValues[STAGE_1_RIGHT.getIndex()]
           || ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.ONE_LOADING;
+        feederState = FeederStates.ONE_LOADING;
       }
       m_feeder.stopBothStages();
       numberOfPowerCellsInFeeder = 0;
@@ -95,7 +94,7 @@ public class DefaultFeederCommand extends CommandBase {
 
     case ONE_LOADING:
       if (ballSensorValues[STAGE_2_BEGINNING_MIDDLE.getIndex()] && !ballSensorValues[STAGE_2_BEGINNING.getIndex()]) {
-        m_feederState = FeederStates.ONE_LOADED;
+        feederState = FeederStates.ONE_LOADED;
       }
       m_feeder.runBothStages(DEFAULT_STAGES_ONE_AND_TWO_SPEED, DEFAULT_STAGE_THREE_SPEED);
       break;
@@ -103,7 +102,7 @@ public class DefaultFeederCommand extends CommandBase {
     case ONE_LOADED:
       if (ballSensorValues[STAGE_1_LEFT.getIndex()] || ballSensorValues[STAGE_1_RIGHT.getIndex()]
           || ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.SECOND_LOADING_1;
+        feederState = FeederStates.SECOND_LOADING_1;
       }
       m_feeder.stopBothStages();
       numberOfPowerCellsInFeeder = 1;
@@ -111,7 +110,7 @@ public class DefaultFeederCommand extends CommandBase {
 
     case SECOND_LOADING_1:
       if (ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.SECOND_LOADING_2;
+        feederState = FeederStates.SECOND_LOADING_2;
       }
       m_feeder.runStageOne(DEFAULT_STAGES_ONE_AND_TWO_SPEED);
       m_feeder.stopStageTwo();
@@ -119,7 +118,7 @@ public class DefaultFeederCommand extends CommandBase {
 
     case SECOND_LOADING_2:
       if (!ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.SECOND_LOADING_3;
+        feederState = FeederStates.SECOND_LOADING_3;
       }
       m_feeder.runStageOne(DEFAULT_STAGES_ONE_AND_TWO_SPEED);
       m_feeder.runStagesTwoAndThree(STAGE_TWO_SLOW_SPEED, STAGE_THREE_SLOW_SPEED);
@@ -127,7 +126,7 @@ public class DefaultFeederCommand extends CommandBase {
 
     case SECOND_LOADING_3:
       if (ballSensorValues[STAGE_2_BEGINNING_MIDDLE.getIndex()] && !ballSensorValues[STAGE_2_BEGINNING.getIndex()]) {
-        m_feederState = FeederStates.SECOND_LOADED;
+        feederState = FeederStates.SECOND_LOADED;
       }
       m_feeder.runBothStages(DEFAULT_STAGES_ONE_AND_TWO_SPEED, DEFAULT_STAGE_THREE_SPEED);
       break;
@@ -135,7 +134,7 @@ public class DefaultFeederCommand extends CommandBase {
     case SECOND_LOADED:
       if (ballSensorValues[STAGE_1_LEFT.getIndex()] || ballSensorValues[STAGE_1_RIGHT.getIndex()]
           || ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.THIRD_LOADING_1;
+        feederState = FeederStates.THIRD_LOADING_1;
       }
       m_feeder.stopBothStages();
       numberOfPowerCellsInFeeder = 2;
@@ -143,7 +142,7 @@ public class DefaultFeederCommand extends CommandBase {
 
     case THIRD_LOADING_1:
       if (ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.THIRD_LOADING_2;
+        feederState = FeederStates.THIRD_LOADING_2;
       }
       m_feeder.runStageOne(DEFAULT_STAGES_ONE_AND_TWO_SPEED);
       m_feeder.stopStageTwo();
@@ -151,14 +150,14 @@ public class DefaultFeederCommand extends CommandBase {
 
     case THIRD_LOADING_2:
       if (!ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.THIRD_LOADING_3;
+        feederState = FeederStates.THIRD_LOADING_3;
       }
       m_feeder.runStageOne(DEFAULT_STAGES_ONE_AND_TWO_SPEED);
       m_feeder.runStagesTwoAndThree(STAGE_TWO_SLOW_SPEED, STAGE_THREE_SLOW_SPEED);
 
     case THIRD_LOADING_3:
       if (ballSensorValues[STAGE_2_BEGINNING.getIndex()] && !ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.THIRD_LOADED;
+        feederState = FeederStates.THIRD_LOADED;
       }
       m_feeder.runStageOne(DEFAULT_STAGES_ONE_AND_TWO_SPEED);
       m_feeder.runStagesTwoAndThree(STAGE_TWO_SLOW_SPEED, STAGE_THREE_SLOW_SPEED);
@@ -167,7 +166,7 @@ public class DefaultFeederCommand extends CommandBase {
     case THIRD_LOADED:
       if (!ballSensorValues[STAGE_2_BEGINNING.getIndex()] && !ballSensorValues[STAGE_2_BEGINNING_MIDDLE.getIndex()]
           && !ballSensorValues[STAGE_1_END.getIndex()]) {
-        m_feederState = FeederStates.EMPTY;
+        feederState = FeederStates.EMPTY;
       }
       m_feeder.stopBothStages();
       numberOfPowerCellsInFeeder = 3;
@@ -193,10 +192,16 @@ public class DefaultFeederCommand extends CommandBase {
     } else {
       m_stageOneLeftRightSensorTriggeredCounter = 0;
     }
-    m_previousState = m_feederState;
 
   }
 
+  /**
+   * This is the safest state to be in because the feeder will 
+   * not auto load any more if it is in third loaded.
+   */
+  public static void setFeederStateToThreeLoaded() {
+    feederState = FeederStates.THIRD_LOADED;
+  }
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
--- src/main/java/frc/robot/commands/ReverseFeeder.java
+++ src/main/java/frc/robot/commands/ReverseFeeder.java
@@ -31,6 +31,7 @@ public class ReverseFeeder extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    DefaultFeederCommand.setFeederStateToThreeLoaded();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
