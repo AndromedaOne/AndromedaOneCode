f7e48a55b69257f061cf3a656185e7963054ced1
SPD3 <sean@ayerdoyles.org>
2021-01-16 15:19:35 -0500

    Calculated angle in the code.

--- src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathA.java
+++ src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathA.java
@@ -15,8 +15,8 @@ public class GalacticSearchPathA extends SequentialCommandGroup {
   private static final double START_TO_E6_POWER_CELL_DISTANCE = 150 * MINIMIZING_FACTOR; // in inches
   private static final double E6_TO_A6_DISTANCE = 120 * MINIMIZING_FACTOR; // in inches
   private static final double A6_TO_FINISH_LINE_DISTANCE = 175 * MINIMIZING_FACTOR; // in inches
-  private static final double ANGLE_TO_LINE_UP_WITH_INITIAL_CELLS = 47.3; // degrees
-  private static final double ANGLE_TO_LINE_UP_WITH_FINAL_CELLS = 49.9; // degrees
+  private static final double ANGLE_TO_LINE_UP_WITH_INITIAL_CELLS = Math.toDegrees(Math.atan(90/120.0)); // degrees
+  private static final double ANGLE_TO_LINE_UP_WITH_FINAL_CELLS = Math.toDegrees(Math.atan(90/150.0)); // degrees
 
   public GalacticSearchPathA(DriveTrain driveTrain, IntakeBase intake) {
     m_driveTrain = driveTrain;
