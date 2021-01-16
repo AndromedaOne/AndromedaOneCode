fae3f37454ad1722918ad4498616c96c6cfb6565
SPD3 <sean@ayerdoyles.org>
2021-01-16 14:50:33 -0500

    Added a max output to test that moveWithEncoder constructor.

--- src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathA.java
+++ src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathA.java
@@ -24,7 +24,7 @@ public class GalacticSearchPathA extends SequentialCommandGroup {
   }
 
   public SequentialCommandGroup getDriveTrainpathWay() {
-    return new SequentialCommandGroup(new MoveUsingEncoder(m_driveTrain, START_TO_E6_POWER_CELL_DISTANCE, true, ANGLE_TO_LINE_UP_WITH_INITIAL_CELLS),
+    return new SequentialCommandGroup(new MoveUsingEncoder(m_driveTrain, START_TO_E6_POWER_CELL_DISTANCE, ANGLE_TO_LINE_UP_WITH_INITIAL_CELLS, 0.4),
         new TurnToCompassHeading(270), new MoveUsingEncoder(m_driveTrain, E6_TO_A6_DISTANCE, true, 270),
         new TurnToCompassHeading(ANGLE_TO_LINE_UP_WITH_FINAL_CELLS),
         new MoveUsingEncoder(m_driveTrain, A6_TO_FINISH_LINE_DISTANCE, true, ANGLE_TO_LINE_UP_WITH_FINAL_CELLS));
