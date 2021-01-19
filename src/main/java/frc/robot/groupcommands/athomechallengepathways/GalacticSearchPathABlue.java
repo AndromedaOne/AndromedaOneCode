f706580849464c0d4c502ff42170460ad1a72c13
SPD3 <sean@ayerdoyles.org>
2021-01-18 21:07:48 -0500

    Created 4 paths for the 4 galactic search options.

---
+++ src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathABlue.java
@@ -0,0 +1,16 @@
+package frc.robot.groupcommands.athomechallengepathways;
+
+import frc.robot.pathgeneration.waypoints.WaypointsBase;
+
+public class GalacticSearchPathABlue extends WaypointsBase{
+
+
+    @Override
+    protected void loadWaypoints() {
+        addWayPoint(AtHomeChallengePoints.C1);
+        addWayPoint(AtHomeChallengePoints.E6);
+        addWayPoint(AtHomeChallengePoints.B7);
+        addWayPoint(AtHomeChallengePoints.D11);
+    }
+    
+}
