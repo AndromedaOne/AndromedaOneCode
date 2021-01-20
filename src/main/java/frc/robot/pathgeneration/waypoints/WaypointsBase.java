7d0cd9d58cd8cc3b238877a3721fda56f268ab54
SPD3 <sean@ayerdoyles.org>
2021-01-20 11:22:06 -0500

    Cleaned up waypointsbase so that waypoints are only loaded once.

--- src/main/java/frc/robot/pathgeneration/waypoints/WaypointsBase.java
+++ src/main/java/frc/robot/pathgeneration/waypoints/WaypointsBase.java
@@ -7,16 +7,21 @@ import java.util.List;
 public abstract class WaypointsBase implements Iterable<Waypoint> {
 
   private List<Waypoint> m_waypoints;
+  private boolean waypointsLoaded;
 
   public WaypointsBase() {
     m_waypoints = new ArrayList<Waypoint>();
+    waypointsLoaded = false;
   }
 
   protected abstract void loadWaypoints();
 
   @Override
   public Iterator<Waypoint> iterator() {
-    loadWaypoints();
+    if(!waypointsLoaded){
+      loadWaypoints();
+      waypointsLoaded = true;
+    }
     return m_waypoints.iterator();
   }
 
