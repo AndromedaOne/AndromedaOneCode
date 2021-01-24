1a5e883c5757278bb16625f583358210ca3671d5
SPD3 <sean@ayerdoyles.org>
2021-01-23 21:24:26 -0500

    Changed the galactic search path to use the waypoint generation.

--- src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathA.java
+++ src/main/java/frc/robot/groupcommands/athomechallengepathways/GalacticSearchPathA.java
@@ -5,30 +5,38 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DeployAndRunIntake;
 import frc.robot.commands.pidcommands.MoveUsingEncoder;
 import frc.robot.commands.pidcommands.TurnToCompassHeading;
+import frc.robot.pathgeneration.pathgenerators.DiagonalPathGenerator;
+import frc.robot.pathgeneration.pathgenerators.DriveTrainDiagonalPathGenerator;
+import frc.robot.pathgeneration.pathgenerators.PathGeneratorBase;
+import frc.robot.pathgeneration.waypoints.Waypoint;
+import frc.robot.pathgeneration.waypoints.WaypointsBase;
 import frc.robot.subsystems.drivetrain.DriveTrain;
 import frc.robot.subsystems.intake.IntakeBase;
 
 public class GalacticSearchPathA extends SequentialCommandGroup {
   private DriveTrain m_driveTrain;
+  private class GalacticSearchWaypoints extends WaypointsBase {
 
-  private static final double MINIMIZING_FACTOR = 1.0;
-  private static final double START_TO_E6_POWER_CELL_DISTANCE = 150 * MINIMIZING_FACTOR; // in inches
-  private static final double E6_TO_A6_DISTANCE = 120 * MINIMIZING_FACTOR; // in inches
-  private static final double A6_TO_FINISH_LINE_DISTANCE = 175 * MINIMIZING_FACTOR; // in inches
-  private static final double ANGLE_TO_LINE_UP_WITH_INITIAL_CELLS = Math.toDegrees(Math.atan(90 / 120.0)); // degrees
-  private static final double ANGLE_TO_LINE_UP_WITH_FINAL_CELLS = Math.toDegrees(Math.atan(90 / 150.0)); // degrees
+    @Override
+    protected void loadWaypoints() {
+      addWayPoint(AtHomeChallengePoints.B1.average(AtHomeChallengePoints.D1));
+      addWayPoint(AtHomeChallengePoints.C3);
+      addWayPoint(AtHomeChallengePoints.D5);
+      addWayPoint(AtHomeChallengePoints.E6);
+      addWayPoint(AtHomeChallengePoints.A6);
+      addWayPoint(AtHomeChallengePoints.B7);
+      addWayPoint(AtHomeChallengePoints.C9);
+      addWayPoint(AtHomeChallengePoints.B11.average(AtHomeChallengePoints.D11));
+    }
+
+  }
+  
 
   public GalacticSearchPathA(DriveTrain driveTrain, IntakeBase intake) {
     m_driveTrain = driveTrain;
-    addCommands(new ParallelDeadlineGroup(getDriveTrainpathWay(), new DeployAndRunIntake(intake, () -> false)));
-  }
-
-  public SequentialCommandGroup getDriveTrainpathWay() {
-    return new SequentialCommandGroup(
-        new MoveUsingEncoder(m_driveTrain, START_TO_E6_POWER_CELL_DISTANCE, ANGLE_TO_LINE_UP_WITH_INITIAL_CELLS, 0.4),
-        new TurnToCompassHeading(270), new MoveUsingEncoder(m_driveTrain, E6_TO_A6_DISTANCE, true, 270),
-        new TurnToCompassHeading(ANGLE_TO_LINE_UP_WITH_FINAL_CELLS),
-        new MoveUsingEncoder(m_driveTrain, A6_TO_FINISH_LINE_DISTANCE, true, ANGLE_TO_LINE_UP_WITH_FINAL_CELLS));
+    Waypoint initialPoint = AtHomeChallengePoints.B1.average(AtHomeChallengePoints.D1);
+    PathGeneratorBase diagonalPathGenerator = new DriveTrainDiagonalPathGenerator(new GalacticSearchWaypoints(), driveTrain,  initialPoint);
+    addCommands(new ParallelDeadlineGroup(diagonalPathGenerator.getPath(), new DeployAndRunIntake(intake, () -> false)));
   }
 
 }
