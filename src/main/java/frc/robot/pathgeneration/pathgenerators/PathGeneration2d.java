4227ef1677c5c9505423d60407e2dd1950c2b1e3
SPD3 <sean@ayerdoyles.org>
2021-02-01 19:37:30 -0500

    Added a reset odometry command

--- src/main/java/frc/robot/pathgeneration/pathgenerators/PathGeneration2d.java
+++ src/main/java/frc/robot/pathgeneration/pathgenerators/PathGeneration2d.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.pathgeneration.waypoints.Waypoint;
 import frc.robot.pathgeneration.waypoints.WaypointWithHeading;
 import frc.robot.pathgeneration.waypoints.WaypointsBase;
@@ -42,17 +43,35 @@ public abstract class PathGeneration2d extends PathGeneratorBase {
         Pose2d initialPose2dPoint = convertInchesWaypointToMeterPose2d(m_initialWaypoint);
         Pose2d finalPose2dPoint = convertInchesWaypointToMeterPose2d(m_finalPoint);
         Trajectory trajectory = TrajectoryGenerator.generateTrajectory(initialPose2dPoint, interiorPoints, finalPose2dPoint, config);
-        return getRamsete(trajectory);
+        ResetOdometryCommand resetOdometryCommand = new ResetOdometryCommand(initialPose2dPoint);
+        SequentialCommandGroup sequentialCommandGroup = new SequentialCommandGroup(resetOdometryCommand, getRamsete(trajectory));
+        return sequentialCommandGroup;
     }
     private Translation2d convertInchesWaypointToMeterTranslation2d(Waypoint waypoint){
         // x and y are reversed because thats what Translation2d expects.
         return new Translation2d(Units.inchesToMeters(waypoint.getY()), Units.inchesToMeters(waypoint.getX()));
     }
     private Pose2d convertInchesWaypointToMeterPose2d(WaypointWithHeading waypoint){
+        // x and y are reversed because thats what Pose2d expects.
         return new Pose2d(Units.inchesToMeters(waypoint.getY()), Units.inchesToMeters(waypoint.getX()), new Rotation2d(waypoint.getHeading()));
     }
     
     protected abstract RamseteCommand getRamsete(Trajectory trajectory);
     protected abstract TrajectoryConfig getConfig();
+    protected abstract void resetOdometry(Pose2d pose);
+    private class ResetOdometryCommand extends CommandBase{
+        private Pose2d m_pose;
+        public ResetOdometryCommand(Pose2d pose){
+            m_pose = pose;
+        }
+        @Override
+        public void initialize(){
+            resetOdometry(m_pose);
+        }
+        @Override
+        public boolean isFinished(){
+            return true;
+        }
+    }
     
 }
