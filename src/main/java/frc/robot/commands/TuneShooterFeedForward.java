234952c1178fa4bbd78d791de37eb72d973c2b46
kjell <kjell.steeman@gmail.com>
2021-02-28 16:47:46 -0500

    created a new command for tuning the shooter.

---
+++ src/main/java/frc/robot/commands/TuneShooterFeedForward.java
@@ -0,0 +1,50 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.commands.pidcommands.RunShooterWheelVelocity;
+import frc.robot.subsystems.shooter.ShooterBase;
+
+public class TuneShooterFeedForward extends CommandBase {
+  /**
+   * Creates a new TuneShooterFeedForward.
+   */
+  private ShooterBase m_shooter;
+  public TuneShooterFeedForward(ShooterBase shooter) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_shooter = shooter;
+    SmartDashboard.putNumber("Feed Forward Value", 0.00025);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    double feedForward = SmartDashboard.getNumber("Feed Forward Value", 0.00025);
+    double shootRPM = SmartDashboard.getNumber("ShooterRPMTarget", 3000);
+    CommandScheduler.getInstance().schedule(new RunShooterWheelVelocity(m_shooter, shootRPM, feedForward));
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
