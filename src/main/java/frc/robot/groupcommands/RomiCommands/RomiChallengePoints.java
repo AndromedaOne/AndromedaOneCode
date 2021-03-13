7f5835948500c4ab13d3f606466ada1d15314d4d
dave <vallieredave@gmail.com>
2021-03-13 15:09:00 -0500

    Initial Romi Challenge Field Points

---
+++ src/main/java/frc/robot/groupcommands/RomiCommands/RomiChallengePoints.java
@@ -0,0 +1,49 @@
+package frc.robot.groupcommands.RomiCommands;
+
+import frc.robot.pathgeneration.waypoints.Waypoint;
+
+public class RomiChallengePoints {
+
+  // Stars on left side
+  public static final Waypoint A0 = new Waypoint(-15, 22.5);
+  public static final Waypoint A1 = new Waypoint(-15, 45);
+  public static final Waypoint A2 = new Waypoint(-15, 67.5);
+
+  
+  public static final Waypoint B0 = new Waypoint(-7.5, 15);
+  public static final Waypoint B1 = new Waypoint(-7.5, 22.5);
+  public static final Waypoint B2 = new Waypoint(-7.5, 30);
+  public static final Waypoint B3 = new Waypoint(-7.5, 37.5);
+  public static final Waypoint B4 = new Waypoint(-7.5, 45);
+  public static final Waypoint B5 = new Waypoint(-7.5, 52.5);
+  public static final Waypoint B6 = new Waypoint(-7.5, 60);
+  public static final Waypoint B7 = new Waypoint(-7.5, 67.5);
+  public static final Waypoint B8 = new Waypoint(-7.5, 75);
+
+
+  public static final Waypoint C0 = new Waypoint(0, 15);
+  public static final Waypoint C1 = new Waypoint(0, 22.5);
+  public static final Waypoint C2 = new Waypoint(0, 30);
+  public static final Waypoint C3 = new Waypoint(0, 37.5);
+  public static final Waypoint C4 = new Waypoint(0, 45);
+  public static final Waypoint C5 = new Waypoint(0, 52.5);
+  public static final Waypoint C6 = new Waypoint(0, 60);
+  public static final Waypoint C7 = new Waypoint(0, 67.5);
+  public static final Waypoint C8 = new Waypoint(0, 75);
+
+  public static final Waypoint D0 = new Waypoint(7.5, 15);
+  public static final Waypoint D1 = new Waypoint(7.5, 22.5);
+  public static final Waypoint D2 = new Waypoint(7.5, 30);
+  public static final Waypoint D3 = new Waypoint(7.5, 37.5);
+  public static final Waypoint D4 = new Waypoint(7.5, 45);
+  public static final Waypoint D5 = new Waypoint(7.5, 52.5);
+  public static final Waypoint D6 = new Waypoint(7.5, 60);
+  public static final Waypoint D7 = new Waypoint(7.5, 67.5);
+  public static final Waypoint D8 = new Waypoint(7.5, 75);
+
+  // Stars along the right side 
+  public static final Waypoint E0 = new Waypoint(15, 22.5);
+  public static final Waypoint E1 = new Waypoint(15, 45);
+  public static final Waypoint E2 = new Waypoint(15, 67.5);
+
+}
