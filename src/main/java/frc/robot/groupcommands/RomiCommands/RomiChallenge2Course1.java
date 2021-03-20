c1892e23c6c08d30a52509ed3f2a946a81043343
kjell <kjell.steeman@gmail.com>
2021-03-20 16:46:24 -0400

    created RomiChallenge2Course1 autonomous and Tuned Romi2.

---
+++ src/main/java/frc/robot/groupcommands/RomiCommands/RomiChallenge2Course1.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.groupcommands.RomiCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.pathgeneration.pathgenerators.DriveTrainDiagonalPathGenerator;
+import frc.robot.pathgeneration.pathgenerators.PathGeneratorBase;
+import frc.robot.pathgeneration.waypoints.Waypoint;
+import frc.robot.pathgeneration.waypoints.WaypointsBase;
+import frc.robot.subsystems.drivetrain.DriveTrain;
+import frc.robot.telemetries.Trace;
+
+public class RomiChallenge2Course1 extends SequentialCommandGroup {
+  private class Course2Waypoints extends WaypointsBase {
+
+    @Override
+    protected void loadWaypoints() {
+      addWayPoint(new Waypoint(0, 41));
+      addWayPoint(new Waypoint(15, 30));
+      addWayPoint(new Waypoint(0, 17.5));
+      addWayPoint(new Waypoint(0, 72.5));
+      addWayPoint(new Waypoint(-15.5, 53));
+      addWayPoint(new Waypoint(-7, 43.5));
+      addWayPoint(new Waypoint(16, 67));
+      addWayPoint(new Waypoint(0, 73));
+      addWayPoint(new Waypoint(0, 0));
+    }
+
+  }
+
+  /** Creates a new RomiChallenge2Course1. */
+  public RomiChallenge2Course1(DriveTrain driveTrain) {
+    PathGeneratorBase path = new DriveTrainDiagonalPathGenerator(getClass().getSimpleName(), new Course2Waypoints(),
+        driveTrain, new Waypoint(0, 0), 0.6, true, true);
+    addCommands(path.getPath());
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Trace.getInstance().logCommandStart(this);
+    super.initialize();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Trace.getInstance().logCommandStop(this);
+    super.end(interrupted);
+  }
+}
--- src/main/java/frc/robot/groupcommands/RomiCommands/RomiChallengePoints.java
+++ src/main/java/frc/robot/groupcommands/RomiCommands/RomiChallengePoints.java
@@ -9,7 +9,6 @@ public class RomiChallengePoints {
   public static final Waypoint A1 = new Waypoint(-15, 45);
   public static final Waypoint A2 = new Waypoint(-15, 67.5);
 
-  
   public static final Waypoint B0 = new Waypoint(-7.5, 15);
   public static final Waypoint B1 = new Waypoint(-7.5, 22.5);
   public static final Waypoint B2 = new Waypoint(-7.5, 30);
@@ -19,7 +18,6 @@ public class RomiChallengePoints {
   public static final Waypoint B6 = new Waypoint(-7.5, 60);
   public static final Waypoint B7 = new Waypoint(-7.5, 67.5);
   public static final Waypoint B8 = new Waypoint(-7.5, 75);
-
 
   public static final Waypoint C0 = new Waypoint(0, 15);
   public static final Waypoint C1 = new Waypoint(0, 22.5);
@@ -41,7 +39,7 @@ public class RomiChallengePoints {
   public static final Waypoint D7 = new Waypoint(7.5, 67.5);
   public static final Waypoint D8 = new Waypoint(7.5, 75);
 
-  // Stars along the right side 
+  // Stars along the right side
   public static final Waypoint E0 = new Waypoint(15, 22.5);
   public static final Waypoint E1 = new Waypoint(15, 45);
   public static final Waypoint E2 = new Waypoint(15, 67.5);
