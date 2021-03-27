b03a6d7dc8b08780701078fe06df0b5a86e11215
kjell <kjell.steeman@gmail.com>
2021-03-27 13:45:33 -0400

    run the intake while driving forward to dislodge the balls.

--- src/main/java/frc/robot/groupcommands/athomechallengepathways/PowerPortContinue.java
+++ src/main/java/frc/robot/groupcommands/athomechallengepathways/PowerPortContinue.java
@@ -10,6 +10,7 @@ package frc.robot.groupcommands.athomechallengepathways;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.RevUpShooter;
+import frc.robot.commands.RunIntakeOut;
 import frc.robot.commands.pidcommands.MoveUsingEncoder;
 import frc.robot.groupcommands.sequentialgroup.DelayedSequentialCommandGroup;
 import frc.robot.subsystems.drivetrain.DriveTrain;
@@ -29,7 +30,7 @@ public class PowerPortContinue extends SequentialCommandGroup {
   public PowerPortContinue(DriveTrain driveTrain, ShooterBase shooter, FeederBase feeder, IntakeBase intake) {
     addCommands(new DelayedSequentialCommandGroup(
         new ParallelDeadlineGroup(new MoveUsingEncoder(driveTrain, reloadToGreen, 0, m_maxOutPut),
-            new RevUpShooter(shooter)),
+            new RevUpShooter(shooter), new RunIntakeOut(intake, () -> false, 1)),
         new PowerPortStart(driveTrain, shooter, feeder, intake)));
   }
 }
