f7997f0f16f3d1848699b0cf6b607d6257463d75
kjell <kjell.steeman@gmail.com>
2021-03-27 13:07:29 -0400

    we added the revup shooter while we doing turn to face.

--- src/main/java/frc/robot/groupcommands/athomechallengepathways/PowerPortStart.java
+++ src/main/java/frc/robot/groupcommands/athomechallengepathways/PowerPortStart.java
@@ -8,9 +8,11 @@
 package frc.robot.groupcommands.athomechallengepathways;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.DefaultFeederCommand;
+import frc.robot.commands.RevUpShooter;
 import frc.robot.commands.pidcommands.MoveUsingEncoder;
 import frc.robot.commands.pidcommands.TurnToFaceCommand;
 import frc.robot.groupcommands.parallelgroup.ShootWithRPM;
@@ -31,9 +33,13 @@ public class PowerPortStart extends SequentialCommandGroup {
   private final double reloadToGreen = reIntroductionZoneDistance - greenZoneShootingDistance - 20;
 
   public PowerPortStart(DriveTrain driveTrain, ShooterBase shooter, FeederBase feeder, IntakeBase intake) {
-    addCommands(new DelayedSequentialCommandGroup(
-        new TurnToFaceCommand(Robot.getInstance().getSensorsContainer().getLimeLight()::horizontalDegreesToTarget),
-        new ShootWithRPM(shooter, feeder, greenZoneShootingRPM), new ParallelCommandGroup(
-            new MoveUsingEncoder(driveTrain, -reloadToGreen, 1.5, m_maxOutPut), new DefaultFeederCommand())));
+    addCommands(
+        new DelayedSequentialCommandGroup(
+            new ParallelDeadlineGroup(
+                new TurnToFaceCommand(
+                    Robot.getInstance().getSensorsContainer().getLimeLight()::horizontalDegreesToTarget),
+                new RevUpShooter(shooter)),
+            new ShootWithRPM(shooter, feeder, greenZoneShootingRPM), new ParallelCommandGroup(
+                new MoveUsingEncoder(driveTrain, -reloadToGreen, 1.5, m_maxOutPut), new DefaultFeederCommand())));
   }
 }
