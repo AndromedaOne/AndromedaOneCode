371c87a8b69468e2a16926c7b6a35e64a4f4533f
SPD3 <sean@ayerdoyles.org>
2021-04-12 20:21:40 -0400

    new paths.

--- src/main/java/frc/robot/oi/SmartDashboard4905.java
+++ src/main/java/frc/robot/oi/SmartDashboard4905.java
@@ -10,6 +10,7 @@ package frc.robot.oi;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.CheckRomiVelocityConversionFactor;
 import frc.robot.commands.ConfigReload;
 import frc.robot.commands.DriveBackwardTimed;
@@ -104,6 +105,24 @@ public class SmartDashboard4905 {
     PathGeneratorBase barrelPathGenerator = new TwoDDriveTrainPathGenerator("RomiBarrelRacing.wpilib.json",
         subsystemsContainer.getDrivetrain(), "RomiBarrelPath");
     SmartDashboard.putData("Romi 2d Barrel", barrelPathGenerator.getPath());
+
+    PathGeneratorBase bouncePathGenerator1 = new TwoDDriveTrainPathGenerator("RomiBounceP1.wpilib.json",
+        subsystemsContainer.getDrivetrain(), "RomiBouncePathP1");
+    PathGeneratorBase bouncePathGenerator2 = new TwoDDriveTrainPathGenerator("RomiBounceP2.wpilib.json",
+        subsystemsContainer.getDrivetrain(), "RomiBouncePathP2");
+    PathGeneratorBase bouncePathGenerator3 = new TwoDDriveTrainPathGenerator("RomiBounceP3.wpilib.json",
+        subsystemsContainer.getDrivetrain(), "RomiBouncePathP3");
+    PathGeneratorBase bouncePathGenerator4 = new TwoDDriveTrainPathGenerator("RomiBounceP4.wpilib.json",
+        subsystemsContainer.getDrivetrain(), "RomiBouncePathP4");
+
+    SequentialCommandGroup bouncePath = new SequentialCommandGroup(
+        bouncePathGenerator1.getPath(),
+        bouncePathGenerator2.getPath(),
+        bouncePathGenerator3.getPath(),
+        bouncePathGenerator4.getPath()
+    );
+
+    SmartDashboard.putData("RomiBounce", bouncePath);
 
     SmartDashboard.putData("CheckRomiVelocityConversionFactor", new CheckRomiVelocityConversionFactor(subsystemsContainer.getDrivetrain()));
 
