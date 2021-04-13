291497c93886f8e3d292ca3c7bd2274c94ed6603
FRC4905 <andromedaonesofrtware@googlegroups.com>
2021-04-12 20:54:44 -0400

    2021 wpilib now has a compressor class which must be instantiated to use the compressor.

---
+++ src/main/java/frc/robot/subsystems/Compressor/RealCompressor.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Compressor;
+
+import edu.wpi.first.wpilibj.Compressor;
+
+/** Add your docs here. */
+public class RealCompressor extends CompressorBase {
+  private Compressor m_compressor;
+
+  public RealCompressor() {
+    m_compressor = new Compressor();
+  }
+
+  public void start() {
+    m_compressor.start();
+  }
+}
