a2f128bb4747231322cd1573b53a119b9b770467
SPD3 <sean@ayerdoyles.org>
2021-06-01 20:32:27 -0400

    added create method to TimedCommand that does not take another command to run with

--- src/main/java/frc/robot/commands/TimedCommand.java
+++ src/main/java/frc/robot/commands/TimedCommand.java
@@ -2,7 +2,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 
 public class TimedCommand extends CommandBase{
@@ -12,8 +11,10 @@ public class TimedCommand extends CommandBase{
     public static CommandBase create(CommandBase command, double time){
         ParallelDeadlineGroup parallelDeadlineGroup = new ParallelDeadlineGroup(new TimedCommand(time), command);
         return parallelDeadlineGroup;
-
     } 
+    public static CommandBase create(double time) {
+        return new TimedCommand(time);
+    }
     private TimedCommand(double time) {
         timer = new Timer();
         m_time = time;
