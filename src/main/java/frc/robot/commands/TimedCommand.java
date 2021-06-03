73d8af29efc7ab4f1a3757ac8dea1d58eef983e7
SPD3 <sean@ayerdoyles.org>
2021-06-03 17:47:43 -0400

    Spotless

--- src/main/java/frc/robot/commands/TimedCommand.java
+++ src/main/java/frc/robot/commands/TimedCommand.java
@@ -4,30 +4,33 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 
-public class TimedCommand extends CommandBase{
-    private Timer timer;
-    private double m_time;
-    
-    public static CommandBase create(CommandBase command, double time){
-        ParallelDeadlineGroup parallelDeadlineGroup = new ParallelDeadlineGroup(new TimedCommand(time), command);
-        return parallelDeadlineGroup;
-    } 
-    public static CommandBase create(double time) {
-        return new TimedCommand(time);
-    }
-    private TimedCommand(double time) {
-        timer = new Timer();
-        m_time = time;
-    }
+public class TimedCommand extends CommandBase {
+  private Timer timer;
+  private double m_time;
 
-    @Override
-    public void initialize() {
-        super.initialize();
-        timer.reset();
-        timer.start();
-    }
-    @Override
-    public boolean isFinished() {
-        return timer.hasElapsed(m_time);
-    }
+  public static CommandBase create(CommandBase command, double time) {
+    ParallelDeadlineGroup parallelDeadlineGroup = new ParallelDeadlineGroup(new TimedCommand(time), command);
+    return parallelDeadlineGroup;
+  }
+
+  public static CommandBase create(double time) {
+    return new TimedCommand(time);
+  }
+
+  private TimedCommand(double time) {
+    timer = new Timer();
+    m_time = time;
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    timer.reset();
+    timer.start();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return timer.hasElapsed(m_time);
+  }
 }
