e1d89d7303ba890b5b598970744733ec46801e23
Seth Valliere <vallierelikesdrwho@gmail.com>
2021-11-22 19:57:25 -0500

    Created Simple Alliance Antics Path

--- src/main/java/frc/robot/commands/ToggleConveyor.java
+++ src/main/java/frc/robot/commands/ToggleConveyor.java
@@ -7,6 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.actuators.ServoMotor;
+import frc.robot.telemetries.Trace;
 
 public class ToggleConveyor extends CommandBase {
   private ServoMotor m_conveyor;
@@ -20,6 +21,7 @@ public class ToggleConveyor extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    Trace.getInstance().logCommandStart(this);
     if (Robot.getInstance().getSubsystemsContainer().getConveyorState()) {
       m_conveyor.stop();
       Robot.getInstance().getSubsystemsContainer().setConveyorState(false);
@@ -37,6 +39,7 @@ public class ToggleConveyor extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    Trace.getInstance().logCommandStop(this);
   }
 
   // Returns true when the command should end.
---
+++ src/main/java/frc/robot/groupcommands/RomiCommands/AllianceAnticsSimple.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.groupcommands.RomiCommands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.pathgeneration.pathgenerators.DriveTrainRectangularPathGenerator;
+import frc.robot.pathgeneration.pathgenerators.PathGeneratorBase;
+import frc.robot.pathgeneration.waypoints.Waypoint;
+import frc.robot.pathgeneration.waypoints.WaypointsBase;
+import frc.robot.subsystems.drivetrain.DriveTrain;
+
+public class AllianceAnticsSimple extends SequentialCommandGroup {
+  private class Waypoints extends WaypointsBase {
+
+    @Override
+    protected void loadWaypoints() {
+      addWayPoint(new Waypoint(0, 48));
+      addWayPoint(new Waypoint(-12, 54));
+
+    }
+
+  }
+
+  /** Creates a new AllianceAnticsSimple. */
+  public AllianceAnticsSimple(DriveTrain driveTrain) {
+    PathGeneratorBase path = new DriveTrainRectangularPathGenerator(new Waypoints(), driveTrain);
+    addCommands(path.getPath());
+  }
+
+}
