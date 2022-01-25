d244fd8ebcaf0085f266275669999e900e779f72
Amfarley33 <amfarley33@gmail.com>
2022-01-24 20:25:13 -0500

    Started climber and subsystem framework.

---
+++ src/main/java/frc/robot/subsystems/climber/ClimberBase.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems.climber;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public abstract class ClimberBase extends SubsystemBase {
+
+  public ClimberBase() {
+  }
+
+  public abstract void extendFrontLeftArm();
+
+  public abstract void extendBackLeftArm();
+
+  public abstract void extendFrontRightArm();
+
+  public abstract void extendBackRightArm();
+
+  public abstract void retractFrontLeftArm();
+
+  public abstract void retractBackLeftArm();
+
+  public abstract void retractFrontRightArm();
+
+  public abstract void retractBackRightArm();
+
+  public abstract void driveFrontLeftWinch();
+
+  public abstract void driveBackLeftWinch();
+
+  public abstract void driveFrontRightWinch();
+
+  public abstract void driveBacktRightWinch();
+
+  public abstract void stopFrontLeftWinch();
+
+  public abstract void stopBackLeftWinch();
+
+  public abstract void stopFrontRightWinch();
+
+  public abstract void stopBacktRightWinch();
+}
---
+++ src/main/java/frc/robot/subsystems/climber/MockClimber.java
@@ -0,0 +1,89 @@
+package frc.robot.subsystems.climber;
+
+public class MockClimber extends ClimberBase {
+
+  public MockClimber() {
+
+  }
+
+  @Override
+  public void extendFrontLeftArm() {
+
+  }
+
+  @Override
+  public void extendBackLeftArm() {
+
+  }
+
+  @Override
+  public void extendFrontRightArm() {
+
+  }
+
+  @Override
+  public void extendBackRightArm() {
+
+  }
+
+  @Override
+  public void retractFrontLeftArm() {
+
+  }
+
+  @Override
+  public void retractBackLeftArm() {
+
+  }
+
+  @Override
+  public void retractFrontRightArm() {
+
+  }
+
+  @Override
+  public void retractBackRightArm() {
+
+  }
+
+  @Override
+  public void driveFrontLeftWinch() {
+
+  }
+
+  @Override
+  public void driveBackLeftWinch() {
+
+  }
+
+  @Override
+  public void driveFrontRightWinch() {
+
+  }
+
+  @Override
+  public void driveBacktRightWinch() {
+
+  }
+
+  @Override
+  public void stopFrontLeftWinch() {
+
+  }
+
+  @Override
+  public void stopBackLeftWinch() {
+
+  }
+
+  @Override
+  public void stopFrontRightWinch() {
+
+  }
+
+  @Override
+  public void stopBacktRightWinch() {
+
+  }
+
+}
