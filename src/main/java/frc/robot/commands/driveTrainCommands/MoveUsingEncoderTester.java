950b72a2276408dc868c55f36e05dba920bc3e7b
bradyarsenault <bradyarsenault08@gmail.com>
2022-02-24 18:47:21 -0500

    Added the ability to drive at the current heading to MoveUsingEncoder

--- src/main/java/frc/robot/commands/driveTrainCommands/MoveUsingEncoderTester.java
+++ src/main/java/frc/robot/commands/driveTrainCommands/MoveUsingEncoderTester.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Robot;
 import frc.robot.commands.ConfigReload;
 import frc.robot.subsystems.drivetrain.DriveTrain;
 import frc.robot.telemetries.Trace;
@@ -19,11 +18,8 @@ public class MoveUsingEncoderTester extends CommandBase {
   @Override
   public void initialize() {
     double distance = SmartDashboard.getNumber("MoveUsingEncoderTester Distance To Move", 24);
-    CommandScheduler.getInstance()
-        .schedule(new SequentialCommandGroup(new ConfigReload(),
-            new MoveUsingEncoder(m_driveTrain, distance,
-                Robot.getInstance().getSensorsContainer().getGyro().getCompassHeading(), 0.7),
-            new TurnDeltaAngle(180)));
+    CommandScheduler.getInstance().schedule(new SequentialCommandGroup(new ConfigReload(),
+        new MoveUsingEncoder(m_driveTrain, distance, 0.7), new TurnDeltaAngle(180)));
     Trace.getInstance().logCommandStart(this);
     Trace.getInstance().logCommandInfo(this, "Moving distance: " + distance + " inches");
   }
