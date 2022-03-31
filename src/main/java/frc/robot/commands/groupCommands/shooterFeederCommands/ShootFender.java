4c882e908b22c2811c34de6f78007edeabf5215d
frc4905laptop <andromedaonesoftware@googlegroups.com>
2022-03-30 20:18:46 -0400

    adjusted angles for the shooting backwards (fender, tarmac, and wall)

--- src/main/java/frc/robot/commands/groupCommands/shooterFeederCommands/ShootFender.java
+++ src/main/java/frc/robot/commands/groupCommands/shooterFeederCommands/ShootFender.java
@@ -30,7 +30,7 @@ public class ShootFender extends SequentialCommandGroup {
     if (Robot.getInstance().getOIContainer().getSubsystemController()
         .getShootBackwardButtonPressed()) {
       m_shooterSetpoint = 3000.0;
-      m_shooterAngle = 59;
+      m_shooterAngle = 61;
       m_feederSetpoint = 1.0;
     } else if (Robot.getInstance().getOIContainer().getSubsystemController()
         .getShootLowHubButtonPressed()) {
