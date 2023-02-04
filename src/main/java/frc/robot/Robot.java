a6d3fa3191d656a75b1cc07718dd8888f5929861
graceirwin <gracielirwin@gmail.com>
2023-02-04 13:07:41 -0500

    Removed lines relating to the limelight table

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,8 +7,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -80,7 +78,6 @@ public class Robot extends TimedRobot {
    * This runs after the mode specific periodic functions, but before LiveWindow
    * and SmartDashboard integrated updating.
    */
-  NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
 
   @Override
   public void robotPeriodic() {
