5804b33f99451d5fb3f29437f26910cd9643829e
Jon7Block <Jon4trains@gmail.com>
2023-03-02 20:49:45 -0500

    Initial creation of EnableParkingBrake. Not tested.

---
+++ src/main/java/frc/robot/commands/driveTrainCommands/EnableParkingBrake.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.driveTrainCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.drivetrain.DriveTrain;
+import frc.robot.telemetries.Trace;
+
+public class EnableParkingBrake extends CommandBase {
+  /** Creates a new EnableParkingBrake. */
+  DriveTrain m_driveTrain;
+
+  public EnableParkingBrake(DriveTrain driveTrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_driveTrain = driveTrain;
+    addRequirements(driveTrain);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Trace.getInstance().logCommandStart(this);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_driveTrain.enableParkingBrakes();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Trace.getInstance().logCommandStop(this);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
