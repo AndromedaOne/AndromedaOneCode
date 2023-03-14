f6c87581981ab14c420227014dd5b83f6daee9ae
Jon7Block <Jon4trains@gmail.com>
2023-03-13 20:59:20 -0400

    Slight Changes to arm. Not tested.

--- src/main/java/frc/robot/commands/groupCommands/samArmRotExtRetCommands/BottomScorePosition.java
+++ src/main/java/frc/robot/commands/groupCommands/samArmRotExtRetCommands/BottomScorePosition.java
@@ -28,8 +28,8 @@ public class BottomScorePosition extends SequentialCommandGroup4905 {
   public BottomScorePosition(SamArmRotateBase armRotate, SamArmExtRetBase armExtRet) {
     addCommands(new SequentialCommandGroup4905(
         new RotateArm(armRotate, ArmRotationExtensionSingleton.getInstance().getAngle(), true),
-    new ExtendRetractInternal(armExtRet, ArmRotationExtensionSingleton.getInstance().getPosition(),
-        true)));
+        new ExtendRetractInternal(armExtRet,
+            ArmRotationExtensionSingleton.getInstance().getPosition(), true)));
 
   }
 
