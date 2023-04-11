e962bbd2eea5667c160c7f865aaa72d3960c374e
William J. Schilp, PhD <williamschilp@gmail.com>
2023-04-10 21:06:47 -0400

    start implementation

---
+++ src/main/java/frc/robot/subsystems/ShowBotAudio/RealShowBotAudio.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.ShowBotAudio;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.telemetries.Trace;
+
+/** Add your docs here. */
+public class RealShowBotAudio extends ShowBotAudioBase {
+  // smartdashboard keys
+  private final String m_audioFileToPlayKey = "showBotAudioFileToPlay";
+  private final String m_currentAudioFilePlayingKey = "showBotAudioFileCurrentlyPlaying";
+  private final String m_audioIsPlayingKey = "showBotAudioIsPlaying";
+  private final String m_showBotPiAudioPlayerConnectedKey = "showBotPiAudioConnected";
+  private final String m_errorStatusKey = "showBotPiAudioErrorStatus";
+  private final String m_stopAudioKey = "showBotStopPiAudio";
+
+  private final String m_showBotPiIsConnected = "ShowBotPiIsConnected";
+  private final String m_roborioAckPiConnected = "RoborioAckPiConnected";
+  private final String m_noError = "";
+
+  private boolean m_audioisConnected = false;
+
+  public RealShowBotAudio() {
+
+  }
+
+  @Override
+  public void playAudio(String file) {
+    if (!m_audioisConnected) {
+      Trace.getInstance().logInfo(
+          "ShowBotAudio: Warning: audio is not conntected. " + "Cannot play file: " + file);
+      return;
+    }
+    SmartDashboard.putString(m_audioFileToPlayKey, file);
+  }
+
+  @Override
+  public void stopAudio() {
+    if (!m_audioisConnected) {
+      Trace.getInstance()
+          .logInfo("ShowBotAudio: Warning: audio is not conntected. " + "Cannot stop audio");
+      return;
+    }
+    SmartDashboard.putBoolean(m_stopAudioKey, isAudioPlaying());
+  }
+
+  @Override
+  public boolean isAudioPlaying() {
+    if (!m_audioisConnected) {
+      Trace.getInstance().logInfo("ShowBotAudio: Warning: audio is not conntected. ");
+      return false;
+    }
+    return SmartDashboard.getBoolean(m_audioFileToPlayKey, isAudioPlaying());
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ShowBotAudio/ShowBotAudioBase.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.ShowBotAudio;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public abstract class ShowBotAudioBase extends SubsystemBase {
+  /** Creates a new ShowBotAudioBase. */
+  public ShowBotAudioBase() {
+  }
+
+  public abstract void playAudio(String file);
+
+  public abstract void stopAudio();
+
+  public abstract boolean isAudioPlaying();
+
+}
