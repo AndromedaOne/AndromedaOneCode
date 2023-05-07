a938c98656a36341260316fb32c95aa0bb0b0204
William J. Schilp, PhD <williamschilp@gmail.com>
2023-05-07 18:50:21 -0400

    change startup audio clip

--- src/main/java/frc/robot/subsystems/showBotAudio/RealShowBotAudio.java
+++ src/main/java/frc/robot/subsystems/showBotAudio/RealShowBotAudio.java
@@ -65,7 +65,7 @@ public class RealShowBotAudio extends ShowBotAudioBase {
         m_audioisConnected = true;
       }
       if (m_audioisConnected) {
-        playAudio(AudioFiles.SolarAnthem);
+        playAudio(AudioFiles.ManhattanProject);
       }
     }
   }
