80b3b1f5f9671a2094c9e3aa0a8c8def0fe8cfe1
graceirwin <gracielirwin@gmail.com>
2023-05-08 19:24:38 -0400

    Fixed Showbot and TopGun so they start in slow mode.
    (cherry picked from commit ea15377d50afb6c04271917073d0341f5737ea2b)
    
    # Conflicts:
    #	src/main/java/frc/robot/subsystems/showBotAudio/RealShowBotAudio.java

--- src/main/java/frc/robot/subsystems/drivetrain/RealDriveTrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/RealDriveTrain.java
@@ -60,6 +60,9 @@ public abstract class RealDriveTrain extends DriveTrain {
       m_hasParkingBrake = false;
       m_parkingBrakeStates = ParkingBrakeStates.BRAKESOFF;
     }
+    if (Config4905.getConfig4905().isShowBot() || Config4905.getConfig4905().isTopGun()) {
+      setDriveTrainMode(DriveTrainMode.SLOW);
+    }
   }
 
   public void init() {
