9959b7103a285d0d49da3738628049847e5dd28f
Andromeda1ASRSD <ayershirleyfirst@gmail.com>
2023-07-17 20:18:19 -0400

    Changed Shoot Three Pointer to two separate commands, ShootLongShot and ShootShortShot.

--- src/main/java/frc/robot/oi/DriveController.java
+++ src/main/java/frc/robot/oi/DriveController.java
@@ -13,7 +13,8 @@ import frc.robot.Config4905;
 import frc.robot.commands.driveTrainCommands.ToggleBrakes;
 import frc.robot.commands.driveTrainCommands.TurnToCompassHeading;
 import frc.robot.commands.groupCommands.topGunShooterFeederCommands.PickUpCargo;
-import frc.robot.commands.groupCommands.topGunShooterFeederCommands.ShootThreePointer;
+import frc.robot.commands.groupCommands.topGunShooterFeederCommands.ShootLongShot;
+import frc.robot.commands.groupCommands.topGunShooterFeederCommands.ShootShortShot;
 import frc.robot.commands.groupCommands.topGunShooterFeederCommands.UnstickCargo;
 import frc.robot.commands.limeLightCommands.ToggleLimelightLED;
 import frc.robot.commands.showBotAudio.PlayAudio;
@@ -108,7 +109,10 @@ public class DriveController extends ControllerBase {
     getBackButton().whileTrue(new UnstickCargo(m_subsystemsContainer.getFeeder(),
         m_subsystemsContainer.getTopShooterWheel(), m_subsystemsContainer.getBottomShooterWheel(),
         m_subsystemsContainer.getShooterAlignment(), m_subsystemsContainer.getIntake()));
-    getXbutton().whileTrue(new ShootThreePointer(m_subsystemsContainer.getFeeder(),
+    getXbutton().whileTrue(new ShootLongShot(m_subsystemsContainer.getFeeder(),
+        m_subsystemsContainer.getTopShooterWheel(), m_subsystemsContainer.getBottomShooterWheel(),
+        m_subsystemsContainer.getShooterAlignment()));
+    getYbutton().whileTrue(new ShootShortShot(m_subsystemsContainer.getFeeder(),
         m_subsystemsContainer.getTopShooterWheel(), m_subsystemsContainer.getBottomShooterWheel(),
         m_subsystemsContainer.getShooterAlignment()));
   }
