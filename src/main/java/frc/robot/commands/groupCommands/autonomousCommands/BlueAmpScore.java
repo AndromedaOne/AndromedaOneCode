b5739b0b4a49ae80f13e22f24c50e2fb67e919eb
AidanMReed <aidanmreed@icloud.com>
2024-01-21 16:31:37 -0500

    Started Adding the Auto Modes for this season

---
+++ src/main/java/frc/robot/commands/groupCommands/autonomousCommands/BlueAmpScore.java
@@ -0,0 +1,27 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.groupCommands.autonomousCommands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Robot;
+import frc.robot.commands.driveTrainCommands.MoveUsingEncoder;
+import frc.robot.subsystems.SubsystemsContainer;
+import frc.robot.subsystems.drivetrain.DriveTrainBase;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class BlueAmpScore extends SequentialCommandGroup {
+  public BlueAmpScore() {
+    // Positioned by the Amp
+    SubsystemsContainer subsystemsContainer = Robot.getInstance().getSubsystemsContainer();
+    DriveTrainBase driveTrain = subsystemsContainer.getDriveTrain();
+    //
+    addCommands(new ParallelDeadlineGroup(
+        new ParallelCommandGroup(new MoveUsingEncoder(driveTrain, -166, 0.5))));
+  }
+}
--- src/main/java/frc/robot/oi/AutoModes4905.java
+++ src/main/java/frc/robot/oi/AutoModes4905.java
@@ -3,6 +3,19 @@ package frc.robot.oi;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Config4905;
+import frc.robot.commands.groupCommands.autonomousCommands.BlueAmpScore;
+import frc.robot.commands.groupCommands.autonomousCommands.BlueCentralSpeaker2Scores;
+import frc.robot.commands.groupCommands.autonomousCommands.BlueCentralSpeaker3Scores;
+import frc.robot.commands.groupCommands.autonomousCommands.BlueDriveStation2Speaker;
+import frc.robot.commands.groupCommands.autonomousCommands.BlueDriveStation3SpeakerWithAmp;
+import frc.robot.commands.groupCommands.autonomousCommands.EmergencyBackup;
+import frc.robot.commands.groupCommands.autonomousCommands.RedAmpScore;
+import frc.robot.commands.groupCommands.autonomousCommands.RedCentralSpeaker2Scores;
+import frc.robot.commands.groupCommands.autonomousCommands.RedCentralSpeaker3Scores;
+import frc.robot.commands.groupCommands.autonomousCommands.RedDriveStation2Speaker;
+import frc.robot.commands.groupCommands.autonomousCommands.RedDriveStation3SpeakerWithAmp;
+import frc.robot.commands.groupCommands.autonomousCommands.TaxiAuto;
 import frc.robot.commands.groupCommands.topGunAutonomousCommands.DoNothingAuto;
 import frc.robot.sensors.SensorsContainer;
 import frc.robot.subsystems.SubsystemsContainer;
@@ -15,6 +28,36 @@ public class AutoModes4905 {
     m_autoChooser = autoChooser;
 
     m_autoChooser.setDefaultOption("DoNothing", new DoNothingAuto());
+
+    if (Config4905.getConfig4905().m_isSwerveBot()) {
+      m_autoChooser.addOption("1: Taxi", new TaxiAuto());
+      m_autoChooser.addOption("2: Red, Score Amp, Pick Up Note, Score Amp", new RedAmpScore());
+      m_autoChooser.addOption(
+          "3: Red Central Speaker Start, Score Speaker, Pick Up Note, Score Speaker",
+          new RedCentralSpeaker2Scores());
+      m_autoChooser.addOption(
+          "4: Red Cental Speaker Start, Score Speaker, Pick Up Note, Score Speaker, Pick Up Note, Score Speaker",
+          new RedCentralSpeaker3Scores());
+      m_autoChooser.addOption("5: Red Drive Station Start, Score Speaker, Pick up Note",
+          new RedDriveStation2Speaker());
+      m_autoChooser.addOption(
+          "6: Red Drive Station Start, Score Speaker, Pick up Note, Score Speaker",
+          new RedDriveStation3SpeakerWithAmp());
+      m_autoChooser.addOption("7: Blue, Score Amp, Pick Up Note, Score Amp", new BlueAmpScore());
+      m_autoChooser.addOption(
+          "8: Blue Central Speaker Start, Score Speaker, Pick Up Note, Score Speaker",
+          new BlueCentralSpeaker2Scores());
+      m_autoChooser.addOption(
+          "9: Blue Cental Speaker Start, Score Speaker, Pick Up Note, Score Speaker, Pick Up Note, Score Speaker",
+          new BlueCentralSpeaker3Scores());
+      m_autoChooser.addOption("10: Blue Drive Station Start, Score Speaker, Pick up Note",
+          new BlueDriveStation2Speaker());
+      m_autoChooser.addOption(
+          "11: Blue Drive Station Start, Score Speaker, Pick up Note, Score Speaker",
+          new BlueDriveStation3SpeakerWithAmp());
+      m_autoChooser.addOption("12: EmergencyBackup", new EmergencyBackup());
+    }
+
     SmartDashboard.putData("autoModes", m_autoChooser);
   }
 }
