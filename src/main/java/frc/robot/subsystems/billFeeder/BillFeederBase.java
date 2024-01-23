d9f2bc938fecf29653c4e2feebb2c713b363af49
graceirwin <gracielirwin@gmail.com>
2024-01-22 20:04:12 -0500

    Created the billthoven feeder configs and subsystem. Untested

--- src/main/java/frc/robot/subsystems/billFeeder/BillFeederBase.java
+++ src/main/java/frc/robot/subsystems/billFeeder/BillFeederBase.java
@@ -3,5 +3,10 @@ package frc.robot.subsystems.billFeeder;
 import frc.robot.subsystems.SubsystemInterface;
 
 public interface BillFeederBase extends SubsystemInterface {
-  // Needs methods
+
+  public abstract void runBillFeeder(double speed);
+
+  public abstract void stopBillFeeder();
+
+  public abstract void runBillFeederInReverse(double speed);
 }
--- src/main/java/frc/robot/subsystems/billFeeder/MockBillFeeder.java
+++ src/main/java/frc/robot/subsystems/billFeeder/MockBillFeeder.java
@@ -4,16 +4,29 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class MockBillFeeder implements BillFeederBase {
+  @Override
+  public void runBillFeeder(double speed) {
 
-  // Needs methods
+  }
+
+  @Override
+  public void stopBillFeeder() {
+
+  }
+
+  @Override
+  public void runBillFeederInReverse(double speed) {
+
+  }
+
   @Override
   public SubsystemBase getSubsystemBase() {
-    throw new UnsupportedOperationException("Unimplemented method 'getSubsystemBase'");
+    throw new UnsupportedOperationException("Method 'getSubsystemBase'");
   }
 
   @Override
   public void setDefaultCommand(Command command) {
-    throw new UnsupportedOperationException("Unimplemented method 'setDefaultCommand'");
+    throw new UnsupportedOperationException("Method 'setDefaultCommand'");
   }
 
 }
