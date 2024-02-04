9304136c79278412fd1740e4e7f20100737841d9
Andromeda1ASRSD <ayershirleyfirst@gmail.com>
2024-02-04 16:41:48 -0500

    Testing Playing With Fusion tof distance sensor

---
+++ src/main/java/frc/robot/sensors/pwfTofDistanceSensor/RealpwfTofDistanceSensor.java
@@ -0,0 +1,16 @@
+package frc.robot.sensors.pwfTofDistanceSensor;
+
+import frc.robot.sensors.RealSensorBase;
+
+public class RealpwfTofDistanceSensor extends RealSensorBase implements pwfTofDistanceSensor{
+  
+  public void pwfTofDistanceSensor(){
+
+  }
+
+  @Override
+  protected void updateSmartDashboard() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'updateSmartDashboard'");
+  }
+}
