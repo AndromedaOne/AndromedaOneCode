a414eac8bf45616ded31368ec29fa86fbafa338e
Giovana <giovanagf04@gmail.com>
2024-03-23 10:57:07 -0400

    Edits to ArmRotateSubsystem

--- src/main/java/frc/robot/subsystems/billArmRotate/RealBillArmRotate.java
+++ src/main/java/frc/robot/subsystems/billArmRotate/RealBillArmRotate.java
@@ -2,8 +2,12 @@ package frc.robot.subsystems.billArmRotate;
 
 import com.typesafe.config.Config;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Config4905;
 import frc.robot.actuators.DoubleSolenoid4905;
@@ -11,7 +15,7 @@ import frc.robot.actuators.SparkMaxController;
 import frc.robot.subsystems.compressor.CompressorBase;
 
 //This was just copied over from RealSamArmRotate so it will need editing
-public class RealBillArmRotate extends SubsystemBase implements BillArmRotateBase {
+public class RealBillArmRotate extends ProfiledPIDSubsystem implements BillArmRotateBase {
   private final SparkMaxController m_motor1;
   private double m_minAngle = 0;
   private double m_maxAngle = 0;
@@ -19,11 +23,20 @@ public class RealBillArmRotate extends SubsystemBase implements BillArmRotateBas
   private DoubleSolenoid4905 m_solenoidBrake;
 
   public RealBillArmRotate(CompressorBase compressorBase) {
+    super(
+        // The ProfiledPIDController used by the subsystem
+        new ProfiledPIDController(
+            0,
+            0,
+            0,
+            // The motion profile constraints
+            new TrapezoidProfile.Constraints(0, 0)));
     Config armrotateConfig = Config4905.getConfig4905().getArmRotateConfig();
     m_motor1 = new SparkMaxController(armrotateConfig, "motor1");
     m_solenoidBrake = new DoubleSolenoid4905(compressorBase, armrotateConfig, "solenoidbrake");
     m_maxAngle = armrotateConfig.getDouble("maxAngle");
     m_minAngle = armrotateConfig.getDouble("minAngle");
+    
   }
 
   @Override
@@ -105,4 +118,14 @@ public class RealBillArmRotate extends SubsystemBase implements BillArmRotateBas
     m_motor1.setBrakeMode();
   }
 
+  @Override
+  protected void useOutput(double output, State setpoint) {
+  
+  }
+
+  @Override
+  protected double getMeasurement() {
+    return getAngle();
+
+}
 }
