5b4c140defb1a36800560c47240029a3e3f90e96
Pete Gravelle <pete@thegravelles.net>
2024-03-28 18:43:11 -0400

    Cleaned up armRotateMotionProfiling.ArmRotateMotionProfiling.
    Cleaned up arm subsystem.
    Added K factors to arm config.

--- src/main/java/frc/robot/subsystems/billArmRotate/RealBillArmRotate.java
+++ src/main/java/frc/robot/subsystems/billArmRotate/RealBillArmRotate.java
@@ -2,13 +2,8 @@ package frc.robot.subsystems.billArmRotate;
 
 import com.typesafe.config.Config;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Config4905;
 import frc.robot.actuators.DoubleSolenoid4905;
@@ -16,36 +11,23 @@ import frc.robot.actuators.SparkMaxController;
 import frc.robot.subsystems.compressor.CompressorBase;
 
 //This was just copied over from RealSamArmRotate so it will need editing
-public class RealBillArmRotate extends ProfiledPIDSubsystem implements BillArmRotateBase {
+public class RealBillArmRotate extends SubsystemBase implements BillArmRotateBase {
   private final SparkMaxController m_motor1;
   private double m_minAngle = 0;
   private double m_maxAngle = 0;
-  private double m_Ks = 0; 
-  private double m_Kg = 0; 
-  private double m_Kv = 0; 
   private BillArmBrakeState m_armAngleBrakeState = BillArmBrakeState.ENGAGEARMBRAKE;
   private DoubleSolenoid4905 m_solenoidBrake;
 
   public RealBillArmRotate(CompressorBase compressorBase) {
-    super(
-        new ProfiledPIDController(0,0,0,
-        new TrapezoidProfile.Constraints(0,0)));
-    // Start arm at rest in neutral position
-    setGoal(0);
-        new ArmFeedforward(m_Ks, m_Kg, m_Kv);
     Config armrotateConfig = Config4905.getConfig4905().getArmRotateConfig();
     m_motor1 = new SparkMaxController(armrotateConfig, "motor1");
     m_solenoidBrake = new DoubleSolenoid4905(compressorBase, armrotateConfig, "solenoidbrake");
     m_maxAngle = armrotateConfig.getDouble("maxAngle");
     m_minAngle = armrotateConfig.getDouble("minAngle");
-    m_Ks = armrotateConfig.getDouble("Ks");
-    m_Kg = armrotateConfig.getDouble("Kg");
-    m_Kv = armrotateConfig.getDouble("Kv");
   }
 
   @Override
   public void periodic() {
-    super.periodic();
     SmartDashboard.putNumber("Measured Arm Angle", getAngle());
     SmartDashboard.putString("Arm Brake State", getBrakeState().toString());
   }
@@ -121,16 +103,6 @@ public class RealBillArmRotate extends ProfiledPIDSubsystem implements BillArmRo
   @Override
   public void enableMotorBrake() {
     m_motor1.setBrakeMode();
-  }
-
-  @Override
-  protected void useOutput(double output, State setpoint) {
-  
-  }
-
-  @Override
-  protected double getMeasurement() {
-    return getAngle();
   }
 
 }
