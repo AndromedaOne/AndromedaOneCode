8c175709c238820f0081380f9390f9626f8ebd53
Andromeda1ASRSD <ayershirleyfirst@gmail.com>
2024-04-29 19:33:27 -0400

    Integrated pigeon onto swerve bot. (Not tested)

---
+++ src/main/java/frc/robot/sensors/gyro/RealPigeonGyroSensor.java
@@ -0,0 +1,136 @@
+package frc.robot.sensors.gyro;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.TimerTask;
+
+import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+import com.typesafe.config.Config;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.Config4905;
+
+public class RealPigeonGyroSensor extends RealGyroBase {
+  // use singleton for the gyro member
+  static private WPI_PigeonIMU m_gyro = null;
+
+  private long kInitializeDelay = 3000;
+  private long kDefaultPeriod = 50;
+  private java.util.Timer m_controlLoop;
+  private Instant m_start;
+  private boolean m_calibrated = false;
+
+  /**
+   * Trys creating the gyro and if it can not then it reports an error to the
+   * DriveStation.
+   */
+  public RealPigeonGyroSensor() {
+    // if we have not created the gyro, do it now...
+    if (m_gyro == null) {
+      try {
+        /* Communicate w/pigeon MXP via the MXP SPI Bus. */
+        /*
+         * Alternatively: I2C.Port.kMXP, SerialPort.Port.kMXP or SerialPort.Port.kUSB
+         */
+        /*
+         * See http://pigeon-mxp.kauailabs.com/guidance/selecting-an-interface/ for
+         * details.
+         */
+        Config conf = Config4905.getConfig4905().getSensorConfig();
+        Config pigeonConfig = conf.getConfig("pigeon");
+        int pigeonId = pigeonConfig.getInt("id");
+        System.out.println("Creating a pigeon Gyro on port: " + pigeonId);
+        /* Alternatives: SPI.Port.kMXP, I2C.Port.kMXP or SerialPort.Port.kUSB */
+        m_gyro = new WPI_PigeonIMU(pigeonId);
+        System.out.println("Created pigeon instance");
+        calibrate();
+      } catch (RuntimeException ex) {
+        DriverStation.reportError("Error instantiating pigeon MXP: " + ex.getMessage(), true);
+      }
+    }
+  }
+
+  private class SetInitialAngleReading extends TimerTask {
+
+    RealPigeonGyroSensor m_pigeon;
+
+    public SetInitialAngleReading(RealPigeonGyroSensor pigeon) {
+      m_pigeon = pigeon;
+    }
+
+    @Override
+    public void run() {
+      System.out.println("Setting Initial Gyro Angle");
+      m_pigeon.setInitialZAngleReading(m_gyro.getAngle());
+      m_pigeon.setInitialXAngleReading(m_gyro.getPitch());
+      m_pigeon.setInitialYAngleReading(m_gyro.getRoll());
+      m_calibrated = true;
+      System.out.println("Gyro is calibrated. Initial Angles: \n\tZangle: " + m_gyro.getAngle()
+          + "\n\tXangle: " + m_gyro.getPitch() + "\n\tYangle: " + m_gyro.getRoll() + "\n");
+      cancel();
+    }
+  }
+
+  @Override
+  public void calibrate() {
+    m_start = Instant.now();
+    m_calibrated = false;
+    m_controlLoop = new java.util.Timer();
+    SetInitialAngleReading task = new SetInitialAngleReading(this);
+    m_controlLoop.schedule(task, kInitializeDelay, kDefaultPeriod);
+  }
+
+  @Override
+  public double getRawZAngle() {
+    if (!m_calibrated && (Duration.between(m_start, Instant.now()).toMillis() > 5000)) {
+      System.out.println(
+          "WARNING: pigeon gyro has not completed calibrating before getRawZangle has been called");
+    }
+    return m_gyro.getAngle();
+  }
+
+  @Override
+  public double getRawXAngle() {
+    if (!m_calibrated && (Duration.between(m_start, Instant.now()).toMillis() > 5000)) {
+      System.out.println(
+          "WARNING: pigeon gyro has not completed calibrating before getRawXangle has been called");
+    }
+    return m_gyro.getPitch();
+  }
+
+  @Override
+  public double getRawYAngle() {
+    if (!m_calibrated && (Duration.between(m_start, Instant.now()).toMillis() > 5000)) {
+      System.out.println(
+          "WARNING: pigeon gyro has not completed calibrating before getRawYangle has been called");
+    }
+    return m_gyro.getRoll();
+  }
+
+  @Override
+  public void reset() {
+    m_gyro.reset();
+
+  }
+
+  @Override
+  public double getAngle() {
+    return getZAngle();
+  }
+
+  @Override
+  public double getRate() {
+    return m_gyro.getRate();
+  }
+
+  @Override
+  public void close() throws Exception {
+
+  }
+
+  @Override
+  public boolean getIsCalibrated() {
+    return m_calibrated;
+  }
+
+}
