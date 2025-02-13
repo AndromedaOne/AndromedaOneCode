f70a76ef1562b6f2f4c076a814a1cfb4a082ba75
giovanagf <giovanagf04@gmail.com>
2025-02-12 20:29:19 -0500

    climber stuff

--- src/main/java/frc/robot/subsystems/sbsdclimber/RealSBSDClimber.java
+++ src/main/java/frc/robot/subsystems/sbsdclimber/RealSBSDClimber.java
@@ -4,5 +4,34 @@
 
 package frc.robot.subsystems.sbsdclimber;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Config4905;
+import frc.robot.actuators.SparkMaxController;
+
 /** Add your docs here. */
-public class RealSBSDClimber {}
+public class RealSBSDClimber extends SubsystemBase implements SBSDClimberBase {
+  private SparkMaxController m_climberWinchMotor; 
+
+  public RealSBSDClimber(){
+
+  }
+
+  @Override
+  public SubsystemBase getSubsystemBase() {
+    return this; 
+  }
+
+  @Override
+  public void setDefaultCommand(Command command) {
+  }
+
+  @Override
+  public void climb(double speed) {
+  }
+
+  @Override
+  public void reverseClimb(double speed) {
+  }
+
+}
