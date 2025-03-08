3b0037af87f73b60615c50b7db403490b73ee7b6
92SKraken <sebastian.p.ives@gmail.com>
2025-03-08 12:36:55 -0500

    Added shell commands

---
+++ src/main/java/frc/robot/commands/sbsdAutoCommands/AutoFinishRight123.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.sbsdAutoCommands;
+
+import frc.robot.rewrittenWPIclasses.SequentialCommandGroup4905;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class AutoFinishRight123 extends SequentialCommandGroup4905 {
+  /** Creates a new AutoFinishLeft4. */
+  public AutoFinishRight123() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands();
+  }
+}
