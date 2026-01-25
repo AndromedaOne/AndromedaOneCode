36a31a2ab9570e92e2ee94f8030cc84f679b6689
andromeda1gracei <gracielirwin@gmail.com>
2026-01-25 12:26:35 -0500

    PoV buttons + bump mode all work while not locking out the driver from movement. going from 0 to 270 does not go in the correct direction...

--- src/main/java/frc/robot/commands/driveTrainCommands/TeleOpCommand.java
+++ src/main/java/frc/robot/commands/driveTrainCommands/TeleOpCommand.java
@@ -29,6 +29,7 @@ public class TeleOpCommand extends Command {
   private double m_savedRobotAngle = 0.0;
   private double m_kProportion = 0.0;
   private boolean m_isStrafe = true;
+  private double m_angleKP = 0.0;
 
   private enum SlowMidFastModeStates {
     FASTMODEBUTTONRELEASED, FASTMODEBUTTONPRESSED, MIDMODEBUTTONRELEASED, MIDMODEBUTTONPRESSED,
@@ -51,6 +52,7 @@ public class TeleOpCommand extends Command {
     m_kDelay = m_drivetrainConfig.getInt("teleop.kdelay");
     m_kProportion = m_drivetrainConfig.getDouble("teleop.kproportion");
     m_robotCentricSup = robotCentricSup;
+    m_angleKP = m_drivetrainConfig.getDouble("SwerveDriveConstants.turningAngleKP");
   }
 
   // use this constructor for TankDrive
@@ -147,6 +149,44 @@ public class TeleOpCommand extends Command {
     // it can't be zero but it needs to be low
     rotateStickValue = MathUtil.clamp(getExponential(rotateStickValue, 4.3, 0.7, .1), -1, 1);
     // do not use moveWithGyro here as we're providing the drive straight correction
+    // putting in the thing I wrote in SDT in here cuz I felt it would fit here
+    // better?
+    if (m_driveController.getBButtonPressed() || m_driveController.getUpArrowPressed()
+        || m_driveController.getLeftArrowPressed() || m_driveController.getDownArrowPressed()
+        || m_driveController.getRightArrowPressed()) {
+      double targetAngle = 0.0;
+      if (m_driveController.getBButtonPressed()) {
+        targetAngle = 45;
+      } else if (m_driveController.getUpArrowPressed()) {
+        targetAngle = 0;
+      } else if (m_driveController.getLeftArrowPressed()) {
+        targetAngle = 270;
+      } else if (m_driveController.getDownArrowPressed()) {
+        targetAngle = 180;
+      } else if (m_driveController.getRightArrowPressed()) {
+        targetAngle = 90;
+      }
+      double error = targetAngle - m_gyro.getAngle();
+      // this does NOT WORK!!! will be fixed on tuesday!!!
+      if (error > 180) {
+        error -= 180;
+        error = -error;
+      } else if (error < -180) {
+        error += 180;
+        error = -error;
+      }
+      SmartDashboard.putNumber("error before kp", error);
+      if (Math.abs(error) <= 2) {
+        error = 0;
+      }
+      error *= m_angleKP;
+      SmartDashboard.putNumber("error after kp", error);
+      rotateStickValue = -MathUtil.clamp(error, -1, 1);
+      // without this, if the robot is moving both before AND after b button is
+      // pressed, the robot will go to the saved angle
+      m_savedRobotAngle = m_gyro.getAngle();
+    }
+    SmartDashboard.putNumber("rotatestickvalue", rotateStickValue);
     if (m_isStrafe) {
       m_driveTrain.move(forwardBackwardStickValue, strafeStickValue, rotateStickValue,
           !m_robotCentricSup.getAsBoolean(), true);
