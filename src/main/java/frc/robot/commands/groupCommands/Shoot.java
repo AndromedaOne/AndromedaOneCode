33e0a9ceb001f8a568b84059770140e13d093fd7
Andromeda1ASRSD <ayershirleyfirst@gmail.com>
2026-03-09 20:47:43 -0400

    new shoot command to put on pathplanner

---
+++ src/main/java/frc/robot/commands/groupCommands/Shoot.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.groupCommands;
+
+import frc.robot.commands.Timer;
+import frc.robot.commands.driveTrainCommands.SwerveDriveSetVelocityToZero;
+import frc.robot.rewrittenWPIclasses.ParallelDeadlineGroup4905;
+import frc.robot.rewrittenWPIclasses.SequentialCommandGroup4905;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class Shoot extends SequentialCommandGroup4905 {
+  /** Creates a new Shoot. */
+
+  public Shoot(long timeInMs) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addCommands(new SwerveDriveSetVelocityToZero(), new ParallelDeadlineGroup4905(
+        new Timer(timeInMs), new ShootBasedOnDistance(), new TurnToHubAndRunEjectBelts()));
+  }
+}
