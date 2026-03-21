3a4d4cb7821a7779f662a5c53180eaa26f949f3c
William J. Schilp, PhD <schilpfamily@gmail.com>
2026-03-21 10:09:40 -0400

    update codebase to wpilib 2026. it does compile, but has not been tested. there are MANY warnings about deprecated stuff
    (cherry picked from commit 72f27b36b24510ca685efbbccdb5c1fdd486a62a)
    
    # Conflicts:
    #	vendordeps/Phoenix6-25.2.1.json
    #	vendordeps/Phoenix6-26.1.0.json
    #	vendordeps/Phoenix6-frc2025-latest.json

--- src/main/java/frc/robot/actuators/SparkMaxController.java
+++ src/main/java/frc/robot/actuators/SparkMaxController.java
@@ -2,12 +2,12 @@ package frc.robot.actuators;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.LimitSwitchConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
