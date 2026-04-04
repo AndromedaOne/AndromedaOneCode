ca5ac3024d22b5fc42c3d5fad4df98c32ef4483a
andromeda1gracei <gracielirwin@gmail.com>
2026-04-04 14:12:18 -0400

    added eject belts stop

--- src/main/java/frc/robot/commands/ejectBeltCommands/EjectBeltLeft.java
+++ src/main/java/frc/robot/commands/ejectBeltCommands/EjectBeltLeft.java
@@ -40,6 +40,7 @@ public class EjectBeltLeft extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    m_ejectBelts.stop();
   }
 
   // Returns true when the command should end.
