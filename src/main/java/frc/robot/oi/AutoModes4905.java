746c9b34d0cbf547c244477a673371c022052f23
andromeda1gracei <gracielirwin@gmail.com>
2026-04-06 20:29:17 -0400

    new auto - starts in front of the hub and collects from the depot

--- src/main/java/frc/robot/oi/AutoModes4905.java
+++ src/main/java/frc/robot/oi/AutoModes4905.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Config4905;
+import frc.robot.commands.autoCommands.HubStartDepot;
 import frc.robot.commands.autoCommands.LeftTrench;
 import frc.robot.commands.autoCommands.LeftTrenchLoop;
 import frc.robot.commands.autoCommands.RightTrench;
@@ -40,6 +41,11 @@ public class AutoModes4905 {
       }
       try {
         m_autoChooser.addOption("Half RightFuelRaid Trench", new RightTrench());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      try {
+        m_autoChooser.addOption("HubStartDepot", new HubStartDepot());
       } catch (Exception e) {
         e.printStackTrace();
       }
